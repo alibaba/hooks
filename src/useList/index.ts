c3954b15a62b95bbcbff67b9b84f6ca81e9e2cfa
troy.lty <troy.lty@antfin.com>
2019-08-23 16:33:23 +0800

    feat: 基本类型增加 key

--- src/useList/index.ts
+++ src/useList/index.ts
@@ -1,19 +1,22 @@
 import { useState, useRef, useCallback } from 'react';
 
 export default <T>(initialValue: T[]) => {
+  // 复杂类型 key 存储器
   const keyMap = useRef(new WeakMap());
   const counterRef = useRef(0);
-  // 如果未简单类型生成 key，需要一个常规 obj，目前暂定简单类型无需 key
-  // const normalMap = useRef<{[key: string]: any}>({});
+  // 简单类型 key 存储器
+  const keyList = useRef<number[]>([]);
 
   // 内部方法
-  const setKey = useCallback((obj: T) => {
+  const setKey = useCallback((obj: T, index?: number) => {
     counterRef.current += 1;
     try {
       keyMap.current.set(obj as Record<string, any>, counterRef.current);
     } catch (e) {
-      // 基本类型无法设置 key
-      // normalMap.current[obj.toString()] = counterRef.current;
+      // 设置 基本类型的 key
+      if (index || index === 0) {
+        keyList.current.splice(index, 0, counterRef.current);
+      }
     }
   }, []);
 
@@ -23,13 +26,13 @@ export default <T>(initialValue: T[]) => {
       const prevKey = keyMap.current.get(prevObj as Record<string, any>);
       keyMap.current.set(newObj as Record<string, any>, prevKey);
     } catch (e) {
-      // normalMap.current[newObj.toString()] = normalMap.current[prevObj.toString()];
+      // 基本类型 key 无需修改顺序
     }
   }, []);
 
   const [list, setList] = useState(() => {
-    initialValue.forEach(ele => {
-      setKey(ele);
+    initialValue.forEach((ele, index) => {
+      setKey(ele, index);
     });
     return initialValue || [];
   });
@@ -38,7 +41,7 @@ export default <T>(initialValue: T[]) => {
     setList(l => {
       const temp = [...l];
       temp.splice(index, 0, obj);
-      setKey(obj);
+      setKey(obj, index);
       return temp;
     });
   };
@@ -46,7 +49,7 @@ export default <T>(initialValue: T[]) => {
   const getKey = (index: number) => {
     try {
       const obj = list[index] || {};
-      return keyMap.current.get(obj);
+      return keyMap.current.get(obj) || keyList.current[index];
     } catch (e) {
       return undefined;
       // return normalMap.current[(list[index]).toString()];
@@ -58,8 +61,8 @@ export default <T>(initialValue: T[]) => {
       const head = l.slice(0, index);
       const tail = l.slice(index);
 
-      obj.forEach(ele => {
-        setKey(ele);
+      obj.forEach((ele, i) => {
+        setKey(ele, index + i);
       });
 
       return head.concat(obj).concat(tail);
@@ -79,6 +82,16 @@ export default <T>(initialValue: T[]) => {
     setList(l => {
       const head = l.slice(0, index);
       const tail = l.slice(index + 1);
+
+      // remove keys if necessary
+      try {
+        const keyHead = keyList.current.slice(0, index);
+        const keyTail = keyList.current.slice(index + 1);
+        keyList.current = keyHead.concat(keyTail);
+      } catch (e) {
+        Promise.reject(e);
+      }
+
       return head.concat(tail);
     });
   };
@@ -91,30 +104,53 @@ export default <T>(initialValue: T[]) => {
       const newList = [...l];
       const temp = newList.filter((_: {}, index: number) => index !== oldIndex);
       temp.splice(newIndex, 0, newList[oldIndex]);
+
+      // move keys if necessary
+      try {
+        const keyTemp = keyList.current.filter((_: {}, index: number) => index !== oldIndex);
+        keyTemp.splice(newIndex, 0, keyList.current[oldIndex]);
+        keyList.current = keyTemp;
+      } catch (e) {
+        Promise.reject(e);
+      }
+
       return temp;
     });
   };
 
   const push = (obj: T) => {
     setList(l => {
-      setKey(obj);
+      setKey(obj, 0);
       return l.concat([obj]);
     });
   };
 
   const pop = () => {
+    // remove keys if necessary
+    try {
+      keyList.current = keyList.current.slice(0, keyList.current.length - 1);
+    } catch (e) {
+      Promise.reject(e);
+    }
+
     setList(l => l.slice(0, l.length - 1));
   };
 
   const unshift = (obj: T) => {
     setList(l => {
-      setKey(obj);
+      setKey(obj, 0);
       return [obj].concat(l);
     });
   };
 
   const shift = () => {
-    setList(l => l.slice(1, l.length - 1));
+    // remove keys if necessary
+    try {
+      keyList.current = keyList.current.slice(1, keyList.current.length);
+    } catch (e) {
+      Promise.reject(e);
+    }
+    setList(l => l.slice(1, l.length));
   };
 
   return {
