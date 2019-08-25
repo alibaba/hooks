52fbef7056f4a4a7d40e71f5dba1f72b48a3720c
卡晨 <guangbo.hgb@antfin.com>
2019-08-25 22:15:22 +0800

    fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
-export default {
-  "extends": "@umijs/fabric/dist/eslint"
+const fabric = require('@umijs/fabric');
+
+module.exports = {
+  ...fabric,
+  rules: {
+    ...fabric.rules,
+    "no-restricted-syntax": "off",
+    "@typescript-eslint/no-object-literal-type-assertion": "off",
+  }
 }
