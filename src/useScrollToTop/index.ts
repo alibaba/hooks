59ddd4bf02a63d3857ba0bd2ad9b32c539805d4d
卡晨 <guangbo.hgb@antfin.com>
2019-08-29 11:53:00 +0800

    useScrollToTop supports more options

--- src/useScrollToTop/index.ts
+++ src/useScrollToTop/index.ts
@@ -1,9 +1,25 @@
-import { useLayoutEffect, useRef } from 'react';
+import { RefObject, useLayoutEffect, useRef } from 'react';
 
-const useScrollToTop = (location?: string) => {
+interface Options {
+  location?: string;
+  behavior?: 'auto' | 'smooth';
+  ref?: RefObject<HTMLElement>;
+  offset?: number;
+}
+
+const useScrollToTop = (options: Options = {}) => {
+  const { location, behavior, ref, offset = 0 } = options;
   useLayoutEffect(
     () => {
-      window.scrollTo(0, 0);
+      let top = offset;
+      if (ref && ref.current) {
+        top += ref.current.getBoundingClientRect().top + window.scrollY;
+      }
+      window.scrollTo({
+        left: 0,
+        top,
+        behavior,
+      });
     },
     location ? [location] : [],
   );
