8975b5d625ad865fe286a41f7e0957f0722187d7
troy.lty <troy.lty@antfin.com>
2019-09-24 19:12:24 +0800

    fix: fix docs anchor

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -16,13 +16,13 @@ const options: IBundleOptions = {
   },
   doc: {
     title: '@umijs/hooks',
-    base: '/base',
+    base: '/hooks',
     repository: 'https://github.com/umijs/hooks',
     theme: 'docz-theme-umi-hooks',
     ignore: ['readme.md', 'changelog.md', 'readme_zh-CN.md', 'contributing.md', 'license.md'],
     themeConfig: {
       codemirrorTheme: 'docz-dark',
-      showPlaygroundEditor: true,
+      // showPlaygroundEditor: true,
       menus: [
         { title: '发布日志', link: 'https://github.com/umijs/hooks/releases' },
         { title: 'Github', link: 'https://github.com/umijs/hooks' },
