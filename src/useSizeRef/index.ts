88d23519e72e0edb5c885c16ee84df7d318e2156
troy.lty <troy.lty@antfin.com>
2019-09-29 21:12:44 +0800

    feat: useSizeRef

---
+++ src/useSizeRef/index.ts
@@ -0,0 +1,44 @@
+import React, { useEffect, useState, useRef, MutableRefObject, useLayoutEffect } from 'react';
+import ResizeObserver from 'resize-observer-polyfill';
+
+type args = HTMLElement | (() => HTMLElement);
+
+function useSizeRef<T = HTMLElement>(
+  ...args: args[]
+): [{ width: number; height: number }, MutableRefObject<T>] {
+  const element = useRef<T>(null);
+  const [state, setState] = useState({ width: 0, height: 0 });
+
+  useLayoutEffect(() => {
+    const hasPassedInRef = args.length === 1;
+    const passedInRef =
+      typeof args[0] === 'function' ? (args[0] as (() => HTMLElement))() : args[0];
+
+    if (hasPassedInRef) {
+      if (!passedInRef) {
+        return () => {};
+      }
+    } else if (!element.current) {
+        return () => {};
+      }
+
+    const resizeObserver = new ResizeObserver(entries => {
+      entries.forEach(entry => {
+        setState({ width: entry.target.clientWidth, height: entry.target.clientHeight });
+      });
+    });
+
+    resizeObserver.observe(
+      hasPassedInRef
+        ? ((passedInRef as unknown) as HTMLElement)
+        : ((element.current as unknown) as HTMLElement),
+    );
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [element.current, typeof args[0] === 'function' ? undefined : args[0]]);
+
+  return [state, element as MutableRefObject<T>];
+}
+
+export default useSizeRef;
