d1aeeffc47bf23ef27e96515e616b14c8312c81f
砖家 <576679268@qq.com>
2019-10-22 16:20:37 +0800

    feat: rename useControlledValue to useControllableValue (#99)
    * feat: rename useControlledValue to useControllableValue

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,20 @@ import useAPI, { configRequest } from './useAPI';
 import useAsync from './useAsync';
 import useLoadMore from './useLoadMore';
 import useSearch from './useSearch';
-import useControlledValue from './useControlledValue';
+import useControllableValue from './useControllableValue';
 import useDynamicList from './useDynamicList';
 import useEventEmitter from './useEventEmitter';
 import useVirtualList from './useVirtualList';
 import { configResponsive, useResponsive } from './useResponsive';
 import useSize from './useSize';
 import useLocalStorageState from './useLocalStorageState';
+
+const useControlledValue: typeof useControllableValue = function (...args) {
+  console.warn(
+    'useControlledValue is deprecated and will be removed in the next major version. Please use useControllableValue instead.',
+  );
+  return useControllableValue(...args);
+};
 
 export {
   useAntdTable,
@@ -18,6 +25,7 @@ export {
   useLoadMore,
   useSearch,
   useControlledValue,
+  useControllableValue,
   useDynamicList,
   useVirtualList,
   useResponsive,
