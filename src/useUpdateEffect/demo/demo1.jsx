801d3d8f13541c9fd8a6fd2918b965f25455c5ae
luchx <wb-lcx595275@antfin.com>
2019-10-29 10:26:24 +0800

    fix: code review

--- src/useUpdateEffect/demo/demo1.jsx
+++ src/useUpdateEffect/demo/demo1.jsx
@@ -1,23 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { message, Button } from 'antd';
 import useUpdateEffect from '..';
 
 export default () => {
   const [count, setCount] = useState(0);
+  const isMounted = useRef(false);
 
   useEffect(() => {
-    const time = setTimeout(() => {
-      setCount(c => c + 1);
-    }, 1000);
-
-    return () => {
-      clearTimeout(time);
-    };
+    if (!isMounted.current) {
+      isMounted.current = true;
+      message.success(`useEffect ${count}`);
+    }
   });
 
   useUpdateEffect(() => {
-    // you can look console count 1 and beyond
-    console.log(`count: ${count}`);
-
+    message.success(`useUpdateEffect ${count}`);
     return () => {
       // do something
     };
@@ -25,8 +22,12 @@ export default () => {
 
   return (
     <div>
-      <p>initial value: 0</p>
-      <p>changed value：{count}</p>
+      <p>count: {count}</p>
+      <p>
+        <Button type="primary" onClick={() => setCount(c => c + 1)}>
+          increase
+        </Button>
+      </p>
     </div>
   );
 };
