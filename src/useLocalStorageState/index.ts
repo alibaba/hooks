45d2532ceca88abdb1f8d574d1ffa89ef652b703
卡晨 <guangbo.hgb@antfin.com>
2019-11-01 18:33:17 +0800

    fix useLocalStorageState type definition

--- src/useLocalStorageState/index.ts
+++ src/useLocalStorageState/index.ts
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 
 export default function useLocalStorageState<T = string>(key: string, defaultValue?: T) {
-  const [state, setState] = useState<T | undefined>(() =>
-    (localStorage.getItem(key) === null ? defaultValue : JSON.parse(localStorage.getItem(key)!)),
+  const [state, setState] = useState(
+    () =>
+      (localStorage.getItem(key) === null
+        ? defaultValue
+        : JSON.parse(localStorage.getItem(key)!)) as T,
   );
   function updateState(value?: T) {
     if (value === undefined) {
       localStorage.removeItem(key);
-      setState(defaultValue);
+      setState(defaultValue as T);
     } else {
       localStorage.setItem(key, JSON.stringify(value));
       setState(value);
