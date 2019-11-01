884b4931758e281c2e9b6870a3e4dc9acbac0f31
DiamondYuan <admin@diamondyuan.com>
2019-11-01 20:28:58 +0800

    fix: fix type of useLocalStorageState

--- src/useLocalStorageState/index.ts
+++ src/useLocalStorageState/index.ts
@@ -1,20 +1,22 @@
 import { useState } from 'react';
 
-export default function useLocalStorageState<T = string>(key: string, defaultValue?: T) {
-  const [state, setState] = useState(
-    () =>
-      (localStorage.getItem(key) === null
-        ? defaultValue
-        : JSON.parse(localStorage.getItem(key)!)) as T,
+function useLocalStorageState<T = undefined>(key: string): [T | undefined, (value?: T) => void];
+function useLocalStorageState<T>(key: string, value: T): [T, (value?: T) => void];
+
+function useLocalStorageState<T>(key: string, defaultValue?: T) {
+  const [state, setState] = useState<T | undefined>(() =>
+    (localStorage.getItem(key) === null ? defaultValue : JSON.parse(localStorage.getItem(key)!)),
   );
   function updateState(value?: T) {
-    if (value === undefined) {
+    if (typeof value === 'undefined') {
       localStorage.removeItem(key);
-      setState(defaultValue as T);
+      setState(defaultValue);
     } else {
       localStorage.setItem(key, JSON.stringify(value));
       setState(value);
     }
   }
-  return [state, updateState] as const;
+  return [state, updateState];
 }
+
+export default useLocalStorageState;
