f6a2d8cde9bed7a2bdd26d1e716b6af81eeb4cbe
troy.lty <troy.lty@antfin.com>
2019-11-03 14:06:07 +0800

    docs: useAPI docs

--- src/useAPI/demo/demo1.jsx
+++ src/useAPI/demo/demo1.jsx
@@ -1,27 +1,17 @@
-import React, { useState, useMemo } from 'react';
-import { Spin, Button } from 'antd';
+import React, { useMemo } from 'react';
+import { Spin } from 'antd';
 import useAPI from '..';
 
 export default () => {
-  const [state, set] = useState(0);
-  const { data, loading, cancel, run } = useAPI({
-    url: `https://randomuser.me/api?id=${state}`,
+  const { data, loading } = useAPI({
+    url: 'https://randomuser.me/api',
   });
   const email = useMemo(() => (((data || {}).results || [])[0] || {}).email, [data]);
   return (
     <>
       <Spin spinning={loading}>
-        <div>id: {state}</div> <div>email: {email}</div>
+        <div>email: {email}</div>
       </Spin>
-      <Button.Group
-        style={{
-          marginTop: 16,
-        }}
-      >
-        <Button onClick={run}>reload</Button>
-        <Button onClick={cancel}>cancel</Button>
-        <Button onClick={() => set(c => c + 1)}>increase id</Button>
-      </Button.Group>
     </>
   );
 };
--- src/useAPI/demo/demo1.tsx
+++ src/useAPI/demo/demo1.tsx
@@ -3,20 +3,14 @@ import { Spin, Button } from 'antd';
 import useAPI from '..';
 
 export default () => {
-  const [state, set] = useState(0);
-  const { data, loading, cancel, run } = useAPI({ url: `https://randomuser.me/api?id=${state}` });
+  const { data, loading } = useAPI({ url: 'https://randomuser.me/api' });
   const email = useMemo(() => (((data || {}).results || [])[0] || {}).email, [data]);
 
   return (
     <>
       <Spin spinning={loading}>
-        <div>id: {state}</div> <div>email: {email}</div>
+        <div>email: {email}</div>
       </Spin>
-      <Button.Group style={{ marginTop: 16 }}>
-        <Button onClick={run}>reload</Button>
-        <Button onClick={cancel}>cancel</Button>
-        <Button onClick={() => set(c => c + 1)}>increase id</Button>
-      </Button.Group>
     </>
   );
 };
