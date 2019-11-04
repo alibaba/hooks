c091df74c6331f807db9ff0b8ef1eb0f64058d6a
卡晨 <guangbo.hgb@antfin.com>
2019-11-04 16:13:53 +0800

    fix ts type definition

--- src/useToggle/index.ts
+++ src/useToggle/index.ts
@@ -28,16 +28,19 @@ function useToggle<T = IState, U = IState>(
   setRight: () => void;
 };
 
-function useToggle(defaultValue: IState = false, reverseValue?: IState) {
-  const [state, setState] = useState(defaultValue);
+function useToggle<D extends IState = IState, R extends IState = IState>(
+  defaultValue: D = false as D,
+  reverseValue?: R,
+) {
+  const [state, setState] = useState<D | R>(defaultValue);
   const reverseValueOrigin = useMemo(
-    () => (reverseValue === undefined ? !defaultValue : reverseValue),
+    () => (reverseValue === undefined ? !defaultValue : reverseValue) as D | R,
     [reverseValue],
   );
 
   // 切换返回值
   const toggle = useCallback(
-    (value?: IState) => {
+    (value?: D | R) => {
       // 强制返回状态值，适用于点击操作
       if (value !== undefined) {
         setState(value);
