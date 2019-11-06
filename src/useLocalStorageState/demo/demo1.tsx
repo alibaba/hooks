6e9a0524c4bdea7f474e9bd0a43b09be14e39bb8
卡晨 <guangbo.hgb@antfin.com>
2019-11-06 11:04:19 +0800

    update useLocalStorageState docs

---
+++ src/useLocalStorageState/demo/demo1.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Input, Button } from 'antd';
+import useLocalStorageState from '..';
+
+export default function () {
+  const [message, setMessage] = useLocalStorageState('user-message', 'Hello~');
+  return (
+    <>
+      <Input
+        value={message}
+        onChange={e => {
+          setMessage(e.target.value);
+        }}
+        placeholder="Please enter some words..."
+        style={{ width: 200, marginRight: 16 }}
+      />
+      <Button
+        onClick={() => {
+          setMessage();
+        }}
+      >
+        Reset
+      </Button>
+    </>
+  );
+}
