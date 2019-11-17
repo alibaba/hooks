9c4aeaad6ca175975f5a6e99c946e07269faf49d
troy.lty <troy.lty@antfin.com>
2019-11-17 21:49:44 +0800

    feat: usePrevious

---
+++ src/usePrevious/demo/demo1.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { Button } from 'antd';
+import usePrevious from '..';
+
+export default () => {
+  const [count, setCount] = useState(0);
+  const previous = usePrevious(count);
+
+  console.log('in demo', previous);
+
+  return (
+    <>
+      <div>counter current value: {count}</div>
+      <div>counter previous value: {previous}</div>
+      <Button.Group>
+        <Button onClick={() => setCount(c => c + 1)}> increase </Button>
+        <Button onClick={() => setCount(c => c - 1)}> decrease </Button>
+      </Button.Group>
+    </>
+  );
+};
