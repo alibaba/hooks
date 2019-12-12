c31e555cc88478c719c9834e10ffacf3408096c5
brickspert <576679268@qq.com>
2019-12-12 17:44:33 +0800

    fix: ts error

--- src/useAPIGroup/index.ts
+++ src/useAPIGroup/index.ts
@@ -4,26 +4,19 @@ import usePaginated from './usePaginated';
 function useAPI(...args: Parameters<typeof usePaginated>): ReturnType<typeof usePaginated>;
 function useAPI(...args: Parameters<typeof useAsync>): ReturnType<typeof useAsync>;
 function useAPI(...args: Parameters<typeof useAsync | typeof usePaginated>) {
+  const [service, options] = args as any;
 
-  const [service, options] = args;
+  const { paginated } = options as any;
 
-  const paginated = (options as any).paginated;
+  const result = useAsync(service, {
+    ...options,
+    manual: paginated || options.manual,
+  });
 
-  const result = useAsync(
-    service,
-    {
-      ...options,
-      manual: paginated || options.manual
-    }
-  );
-
-  const paginatedResult = usePaginated(
-    service,
-    {
-      ...options,
-      manual: !paginated || options.manual
-    }
-  );
+  const paginatedResult = usePaginated(service, {
+    ...options,
+    manual: !paginated || options.manual,
+  });
 
   return paginated ? paginatedResult : result;
 }
