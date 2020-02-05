d62c74b6ff7bcf18ff9e3f6f566de132a3950cc9
卡晨 <guangbo.hgb@antfin.com>
2020-02-05 15:18:02 +0800

    doc: useClickAway

--- packages/hooks/src/useClickAway/demo/demo2.tsx
+++ packages/hooks/src/useClickAway/demo/demo2.tsx
@@ -1,3 +1,11 @@
+/**
+ * title: Custom DOM
+ * desc: Support pass in a DOM element directly or a function that returns the DOM element.
+ *
+ * title.zh-CN: 自定义 DOM
+ * desc.zh-CN: 支持直接传入 DOM 对象或通过 function 返回一个对象的方式引入。
+ */
+
 import React, { useState } from 'react';
 import { Button } from 'antd';
 import useClickAway from '..';
