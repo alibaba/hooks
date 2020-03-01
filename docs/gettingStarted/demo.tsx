032e5a77e1aaa5e48c21bfe3f11f9e21d7e103f4
brickspert <576679268@qq.com>
2020-03-01 21:16:08 +0800

    doc: add getting started and faq

---
+++ docs/gettingStarted/demo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => (
+  <iframe
+    src="https://codesandbox.io/embed/jovial-smoke-jx7o1?fontsize=14&hidenavigation=1&theme=dark"
+    style={{ width: '100%', height: 500, border: 0, borderRadius: 4, overflow: 'hidden' }}
+    title="demo for umi hooks"
+    allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
+    sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+  >
+  </iframe>
+)
