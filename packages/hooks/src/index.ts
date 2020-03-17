c891dbd65f9325a59e67da3c548ba289ebb55d76
zhanlipeng <zhanlipeng2014@xiaochuankeji.cn>
2020-03-17 15:09:55 +0800

    add useDocumentVisibility

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -17,6 +17,7 @@ import useUpdateLayoutEffect from './useUpdateLayoutEffect';
 import usePagination from './usePagination';
 import useBoolean from './useBoolean';
 import useToggle from './useToggle';
+import useDocumentVisibility from './useDocumentVisibility';
 import useSelections from './useSelections';
 import useThrottle from './useThrottle';
 import useThrottleFn from './useThrottleFn';
@@ -70,6 +71,7 @@ export {
   usePagination,
   useBoolean,
   useToggle,
+  useDocumentVisibility,
   useSelections,
   useThrottle,
   useThrottleFn,
