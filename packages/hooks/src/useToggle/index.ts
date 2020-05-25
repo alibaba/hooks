1e4018105ce6418e2e5757122869137363184b8d
稻草 <ar227752@alibaba-inc.com>
2020-05-25 14:50:38 +0800

    fix: 简化useMemo依赖项，优化声明类型

--- packages/hooks/src/useToggle/index.ts
+++ packages/hooks/src/useToggle/index.ts
@@ -12,12 +12,12 @@ function useToggle<T = boolean | undefined>(): [boolean, Actions];
 
 function useToggle<T = IState>(
   defaultValue: T,
-): [T, Actions];
+): [T, Actions<T>];
 
 function useToggle<T = IState, U = IState>(
   defaultValue: T,
   reverseValue: U,
-): [T | U, Actions];
+): [T | U, Actions<T | U>];
 
 function useToggle<D extends IState = IState, R extends IState = IState>(
   defaultValue: D = false as D,
@@ -33,25 +33,24 @@ function useToggle<D extends IState = IState, R extends IState = IState>(
 
   const actions = useMemo(() => {
     // 切换返回值
-    const toggle: () => void = (value?: D | R) => {
+    const toggle = (value?: D | R) => {
       // 强制返回状态值，适用于点击操作
       if (value !== undefined) {
         setState(value);
         return;
       }
-      const data = state === defaultValue ? reverseValueOrigin : defaultValue;
-      setState(data);
+      setState(s => s === defaultValue ? reverseValueOrigin : defaultValue);
     }
     // 设置默认值
-    const setLeft: () => void = () => setState(defaultValue);
+    const setLeft = () => setState(defaultValue);
     // 设置取反值
-    const setRight: () => void = () => setState(reverseValueOrigin);
+    const setRight = () => setState(reverseValueOrigin);
     return {
       toggle,
       setLeft,
       setRight,
     }
-  }, [setState, state]);
+  }, [setState]);
 
   return [
     state,
