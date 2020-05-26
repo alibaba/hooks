609e8fed9d7694e303c9e0db0c91020a4c605ab6
卡晨 <guangbo.hgb@antfin.com>
2020-05-26 17:37:27 +0800

    fix useDebounce and useDebounceFn tests

--- packages/hooks/src/useDebounce/__tests__/index.test.ts
+++ packages/hooks/src/useDebounce/__tests__/index.test.ts
@@ -6,51 +6,38 @@ interface ParamsObj {
   wait: number;
 }
 
-/* 暂时关闭 act 警告  见：https://github.com/testing-library/react-testing-library/issues/281#issuecomment-480349256 */
-const originalError = console.error;
-beforeAll(() => {
-  console.error = (...args: any) => {
-    if (/Warning.*not wrapped in act/.test(args[0])) {
-      return;
-    }
-    originalError.call(console, ...args);
-  };
-});
-afterAll(() => {
-  console.error = originalError;
-});
+function sleep(time: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, time)
+  });
+}
 
 let hook: RenderHookResult<ParamsObj, any>;
 
 describe('useDebounce', () => {
-  beforeEach(() => {
-    jest.useFakeTimers();
-  });
-
-  afterEach(() => {
-    jest.useRealTimers();
-  });
   it('should be defined', () => {
     expect(useDebounce).toBeDefined();
   });
 
-  it('useDebounce should work', () => {
+  it('useDebounce should work', async () => {
     let mountedState = 1;
     act(() => {
-      hook = renderHook(() => useDebounce(mountedState, 500));
+      hook = renderHook(() => useDebounce(mountedState, {wait: 200}));
     });
-    act(() => {
+    await act(async () => {
       expect(hook.result.current).toEqual(1);
       mountedState = 2;
       hook.rerender();
       mountedState = 3;
       hook.rerender();
-      jest.runAllTimers();
+      await sleep(300);
       expect(hook.result.current).toEqual(3);
       mountedState = 4;
       hook.rerender();
       expect(hook.result.current).toEqual(3);
-      jest.runAllTimers();
+      await sleep(300);
       expect(hook.result.current).toEqual(4);
     });
   });
--- packages/hooks/src/useDebounceFn/__tests__/index.test.ts
+++ packages/hooks/src/useDebounceFn/__tests__/index.test.ts
@@ -7,19 +7,13 @@ interface ParamsObj {
   wait: number;
 }
 
-/* 暂时关闭 act 警告  见：https://github.com/testing-library/react-testing-library/issues/281#issuecomment-480349256 */
-const originalError = console.error;
-beforeAll(() => {
-  console.error = (...args: any) => {
-    if (/Warning.*not wrapped in act/.test(args[0])) {
-      return;
-    }
-    originalError.call(console, ...args);
-  };
-});
-afterAll(() => {
-  console.error = originalError;
-});
+function sleep(time: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, time)
+  });
+}
 
 let count = 0;
 const debounceFn = (gap: number) => {
@@ -31,37 +25,32 @@ const setUp = ({ fn, wait }: ParamsObj) => renderHook(() => useDebounceFn(fn, {w
 let hook: RenderHookResult<ParamsObj, ReturnType<typeof useDebounceFn>>;
 
 describe('useDebounceFn', () => {
-  beforeEach(() => {
-    jest.useFakeTimers();
-  });
-
-  afterEach(() => {
-    jest.useRealTimers();
-  });
   it('should be defined', () => {
     expect(useDebounceFn).toBeDefined();
   });
 
-  it('run and cancel should work', () => {
+  it('run and cancel should work', async () => {
     act(() => {
       hook = setUp({
         fn: debounceFn,
-        wait: 500,
+        wait: 200,
       });
     });
-    act(() => {
+    await act(async () => {
       hook.result.current.run(2);
       hook.result.current.run(2);
       hook.result.current.run(2);
       hook.result.current.run(2);
-      jest.runAllTimers();
+      expect(count).toBe(0);
+      await sleep(300)
       expect(count).toBe(2);
       hook.result.current.run(4);
-      jest.runAllTimers();
+      expect(count).toBe(2);
+      await sleep(300)
       expect(count).toBe(6);
       hook.result.current.run(4);
       hook.result.current.cancel();
-      jest.runAllTimers();
+      await sleep(300)
       expect(count).toBe(6);
     });
   });
