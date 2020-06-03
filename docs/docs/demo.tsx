a46258cf4ce59f49354d656f6207434d40586105
尽龙 <brickspert.fjl@antfin.com>
2020-06-03 18:45:30 +0800

    doc: add api standards

--- docs/docs/demo.tsx
+++ docs/docs/demo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => (
   <iframe
-    src="https://codesandbox.io/embed/jovial-smoke-jx7o1?fontsize=14&hidenavigation=1&theme=dark"
+    src="https://codesandbox.io/embed/demo-for-ahooks-t7o7v?fontsize=14&hidenavigation=1&theme=dark"
     style={{ width: '100%', height: 500, border: 0, borderRadius: 4, overflow: 'hidden' }}
     title="demo for ahooks"
     allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
