8f85ba4b5ed5949ac3ab4d84e11c3ee4da68ef6c
尽龙 <brickspert.fjl@antfin.com>
2020-06-21 14:36:31 +0800

    fix: useRequst persist function bug

--- packages/use-request/src/utils/usePersistFn.ts
+++ packages/use-request/src/utils/usePersistFn.ts
@@ -1,20 +1,21 @@
-import { useCallback, useRef, useEffect } from 'react';
+import { useCallback, useRef } from 'react';
 
-function usePersistFn(fn: any, dependencies: any = []) {
+function usePersistFn(fn: any) {
   const ref = useRef<any>(() => {
     throw new Error('Cannot call an event handler while rendering.');
   });
 
-  useEffect(() => {
-    ref.current = fn;
-  }, [fn, ...dependencies]);
+  ref.current = fn;
 
-  const persist = useCallback((...args) => {
-    const refFn = ref.current;
-    if (refFn) {
-      return refFn(...args);
-    }
-  }, [ref]);
+  const persist = useCallback(
+    (...args) => {
+      const refFn = ref.current;
+      if (refFn) {
+        return refFn(...args);
+      }
+    },
+    [ref],
+  );
 
   if (typeof fn === 'function') {
     return persist;
