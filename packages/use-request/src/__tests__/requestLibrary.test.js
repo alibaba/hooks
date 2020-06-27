ee9621d824e8bb3665a17ded34ab6b624ac9c268
尽龙 <brickspert.fjl@antfin.com>
2020-06-27 23:22:30 +0800

    test: fix useRequest test case

--- packages/use-request/src/__tests__/requestLibrary.test.js
+++ packages/use-request/src/__tests__/requestLibrary.test.js
@@ -1,36 +1,18 @@
-import createTestServer from 'create-test-server';
-import { extend } from 'umi-request';
 import { renderHook } from '@testing-library/react-hooks';
+import { enableFetchMocks } from 'jest-fetch-mock';
 import useRequest from '../index';
 
-const request = extend({
-  errorHandler(e) {
-    throw e;
-  },
-});
+enableFetchMocks();
 
 describe('request library', () => {
-  let server;
-
-  beforeAll(async () => {
-    server = await createTestServer();
+  beforeEach(() => {
+    fetch.resetMocks();
   });
-
-  afterAll(() => {
-    server.close();
-  });
-
-  const prefix = api => `${server.url}${api}`;
 
   test('success with url', async () => {
-    const rawData = {
-      text: 'testtext',
-    };
-    server.get('/test/success', (req, res) => {
-      res.send(rawData);
-    });
+    fetch.mockResponseOnce(JSON.stringify({ text: 'testtext' }));
 
-    const { result, waitForNextUpdate } = renderHook(() => useRequest(prefix('/test/success')));
+    const { result, waitForNextUpdate } = renderHook(() => useRequest('/test/success'));
     await waitForNextUpdate(() => result.current.data);
     expect(result.current.data).toEqual({
       text: 'testtext',
@@ -38,14 +20,9 @@ describe('request library', () => {
   });
 
   test('success with function return url', async () => {
-    const rawData = {
-      text: 'testtext',
-    };
-    server.get('/test/success', (req, res) => {
-      res.send(rawData);
-    });
+    fetch.mockResponseOnce(JSON.stringify({ text: 'testtext' }));
 
-    const { result, waitForNextUpdate } = renderHook(() => useRequest(() => prefix('/test/success')));
+    const { result, waitForNextUpdate } = renderHook(() => useRequest(() => '/test/success'));
     await waitForNextUpdate(() => result.current.data);
     expect(result.current.data).toEqual({
       text: 'testtext',
@@ -53,14 +30,11 @@ describe('request library', () => {
   });
 
   test('success with object', async () => {
-    const rawData = {
-      text: 'testtext',
-    };
-    server.get('/test/success', (req, res) => {
-      res.send(rawData);
-    });
+    fetch.mockResponseOnce(JSON.stringify({ text: 'testtext' }));
 
-    const { result, waitForNextUpdate } = renderHook(() => useRequest({ url: prefix('/test/success'), method: 'GET' }));
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useRequest({ url: '/test/success', method: 'GET' }),
+    );
     await waitForNextUpdate(() => result.current.data);
     expect(result.current.data).toEqual({
       text: 'testtext',
@@ -68,59 +42,14 @@ describe('request library', () => {
   });
 
   test('success with function return object', async () => {
-    const rawData = {
-      text: 'testtext',
-    };
-    server.get('/test/success', (req, res) => {
-      res.send(rawData);
-    });
+    fetch.mockResponseOnce(JSON.stringify({ text: 'testtext' }));
 
-    const { result, waitForNextUpdate } = renderHook(() => useRequest(() => ({ url: prefix('/test/success'), method: 'GET' })));
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useRequest(() => ({ url: '/test/success', method: 'GET' })),
+    );
     await waitForNextUpdate(() => result.current.data);
     expect(result.current.data).toEqual({
       text: 'testtext',
     });
-  });
-
-  test('http failed with request', async () => {
-    const rawData = {
-      text: 'testtext',
-    };
-    server.get('/test/failed1', (req, res) => {
-      res.status(500);
-      res.send(rawData);
-    });
-
-    const { result, waitForNextUpdate } = renderHook(() =>
-      useRequest(() => request(prefix('/test/failed1')), {
-        requestMethod: request,
-      }),
-    );
-    await waitForNextUpdate(() => result.current.error);
-    expect(result.current.error.message).toEqual('http error');
-  });
-
-  test('http failed with custom request', async () => {
-    const { result, waitForNextUpdate } = renderHook(() =>
-      useRequest(() => {
-        throw new Error('test error');
-      }),
-    );
-    await waitForNextUpdate(() => result.current.error);
-    expect(result.current.error.message).toEqual('test error');
-  });
-
-  test('http failed with url', async () => {
-    const rawData = {
-      text: 'testtext',
-    };
-    server.get('/test/failed3', (req, res) => {
-      res.status(500);
-      res.send(rawData);
-    });
-
-    const { result, waitForNextUpdate } = renderHook(() => useRequest(prefix('/test/failed3')));
-    await waitForNextUpdate(() => result.current.error);
-    expect(result.current.error.message).toEqual('Internal Server Error');
   });
 });
