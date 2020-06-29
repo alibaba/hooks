4993c1d1ba8cbc674d620515d474f48fd9ab77c0
尽龙 <brickspert.fjl@antfin.com>
2020-06-29 20:03:28 +0800

    feat: add useSetState

---
+++ packages/hooks/src/useSetState/demo/demo1.tsx
@@ -0,0 +1,38 @@
+/**
+ * title: Default usage
+ *
+ * title.zh-CN: 基本用法
+ */
+
+import React from 'react';
+import useSetState from '..';
+
+interface State {
+  hello: string;
+  count: number;
+  [key: string]: any;
+}
+
+export default () => {
+  const [state, setState] = useSetState<State>({
+    hello: '',
+    count: 0,
+  });
+
+  return (
+    <div>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+      <p>
+        <button type="button" onClick={() => setState({ hello: 'world' })}>
+          set hello
+        </button>
+        <button type="button" onClick={() => setState({ foo: 'bar' })} style={{ margin: '0 16px' }}>
+          set foo
+        </button>
+        <button type="button" onClick={() => setState((prev) => ({ count: prev.count + 1 }))}>
+          count + 1
+        </button>
+      </p>
+    </div>
+  );
+};
---
+++ packages/hooks/src/useSetState/index.en-US.md
@@ -0,0 +1,25 @@
+---
+title: useSetState
+nav:
+  title: Hooks
+  path: /hooks
+group:
+  title: State
+  path: /state
+---
+
+# useSetState
+
+useSetState works similar to `this.setState` of class component, used to manage the state of object type.
+
+## Examples
+
+<code src="./demo/demo1.tsx" />
+
+## API
+
+```typescript
+const [state, setState] = useSetState<T extends object>(
+  initialState: T = {} as T
+): [T, (patch: Partial<T> | ((prevState: T) => Partial<T>)) => void]
+```
