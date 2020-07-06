3fdb249d856330703ccc334253c9f0adbee280ca
邱可新 <wb-qkx400447@antfin.com>
2020-07-06 15:00:40 +0800

    fix: simplify code

--- packages/hooks/src/useInterval/demo/demo1.tsx
+++ packages/hooks/src/useInterval/demo/demo1.tsx
@@ -12,13 +12,9 @@ import { useInterval } from 'ahooks';
 export default () => {
   const [count, setCount] = useState(0);
 
-  useInterval(
-    () => {
-      setCount(count + 1);
-    },
-    1000,
-    { immediate: false },
-  );
+  useInterval(() => {
+    setCount(count + 1);
+  }, 1000);
 
   return (
     <div>
