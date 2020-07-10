5eb73bc314e20aa599fc6755aa691791b76d4f16
Jialiang T <rememhe@outlook.com>
2020-07-10 14:20:17 +0800

    doc(useEasyReducer): update api demo

--- packages/hooks/src/useEasyReducer/demo/demo1.tsx
+++ packages/hooks/src/useEasyReducer/demo/demo1.tsx
@@ -1,9 +1,7 @@
 /**
  * title: Default usage
- * desc: Use ref to set elements that need listen dom.
  *
  * title.zh-CN: 基本用法
- * desc.zh-CN: 使用 ref 设置需要需要监听的元素。
  */
 
 import * as React from 'react';
