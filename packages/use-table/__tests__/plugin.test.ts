1eeb7d2040f7f2876d8b480640a786a196a277a5
monkindey <monkindey@163.com>
2020-07-20 19:06:27 +0800

    add plugin test

---
+++ packages/use-table/__tests__/plugin.test.ts
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+import { renderHook, act } from '@testing-library/react-hooks';
+import useTable, { Obj } from '../src/index';
+import service from './fixtures/service';
+
+describe('useTable#plugin', () => {
+  it('middlewares', async () => {
+    const dataSource = [{ name: 'ahooks' }];
+    const updatedDataSource = [{ name: 'updated' }];
+    const TOTAL = 25;
+    const plugin = {
+      middlewares: (ctx, next) => {
+        expect(ctx.params).toEqual({ current: 1, pageSize: 20 });
+        return next().then(() => {
+          ctx.response.data.dataSource = updatedDataSource;
+        });
+      },
+    };
+    const { waitForNextUpdate, result } = renderHook(() =>
+      useTable(() => service({ dataSource, total: TOTAL }), { plugins: [plugin] }),
+    );
+
+    await waitForNextUpdate();
+    await waitForNextUpdate();
+
+    expect(result.current.tableProps.dataSource).not.toEqual(dataSource);
+    expect(result.current.tableProps.dataSource).toEqual(updatedDataSource);
+  });
+
+  it('tableProps', async () => {
+    const dataSource = [{ name: 'ahooks' }];
+    const TOTAL = 25;
+    const plugin = {
+      props: {
+        tableProps: { isTree: true },
+      },
+    };
+    const { waitForNextUpdate, result } = renderHook(() =>
+      useTable(() => service({ dataSource, total: TOTAL }), { plugins: [plugin] }),
+    );
+
+    await waitForNextUpdate();
+    await waitForNextUpdate();
+
+    expect((result.current.tableProps as Obj).isTree).toEqual(true);
+  });
+
+  it('external props', async () => {
+    const dataSource = [{ name: 'ahooks' }];
+    const TOTAL = 25;
+    const plugin = {
+      props: {
+        test: true,
+      },
+    };
+    const { waitForNextUpdate, result } = renderHook(() =>
+      useTable(() => service({ dataSource, total: TOTAL }), { plugins: [plugin] }),
+    );
+
+    await waitForNextUpdate();
+    await waitForNextUpdate();
+
+    expect(result.current.test).toEqual(true);
+  });
+
+  it('all', async () => {
+    const dataSource = [{ name: 'ahooks' }];
+    const TOTAL = 25;
+    const usePlugin = () => {
+      const [state, setState] = useState(true);
+      return {
+        props: {
+          state,
+          setState,
+        },
+      };
+    };
+    const { waitForNextUpdate, result } = renderHook(() => {
+      const plugin = usePlugin();
+      return useTable(() => service({ dataSource, total: TOTAL }), { plugins: [plugin] });
+    });
+
+    await waitForNextUpdate();
+    await waitForNextUpdate();
+
+    expect(result.current.state).toEqual(true);
+    act(() => {
+      result.current.setState(false);
+    });
+    expect(result.current.state).toEqual(false);
+  });
+});
