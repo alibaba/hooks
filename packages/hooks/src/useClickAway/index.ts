bc5531bd6693a1e8beb9c932bbd3016ff1aa8248
尽龙 <brickspert.fjl@antfin.com>
2020-09-11 16:28:48 +0800

    optimize: useClickAway

--- packages/hooks/src/useClickAway/index.ts
+++ packages/hooks/src/useClickAway/index.ts
@@ -1,5 +1,5 @@
-import { useEffect, useCallback } from 'react';
-import { getTargetElement, BasicTarget } from '../utils/dom';
+import { useEffect, useRef } from 'react';
+import { BasicTarget, getTargetElement } from '../utils/dom';
 
 // 鼠标点击事件，click 不会监听右键
 const defaultEvent = 'click';
@@ -11,10 +11,15 @@ export default function useClickAway(
   target: BasicTarget | BasicTarget[],
   eventName: string = defaultEvent,
 ) {
-  const handler = useCallback(
-    (event) => {
-      const targets = Array.isArray(target) ? target : [target];
+  const onClickAwayRef = useRef(onClickAway);
+  onClickAwayRef.current = onClickAway;
 
+  const targetRef = useRef(target);
+  targetRef.current = target;
+
+  useEffect(() => {
+    const handler = (event: any) => {
+      const targets = Array.isArray(targetRef.current) ? targetRef.current : [targetRef.current];
       if (
         targets.some((targetItem) => {
           const targetElement = getTargetElement(targetItem) as HTMLElement;
@@ -23,17 +28,13 @@ export default function useClickAway(
       ) {
         return;
       }
-
       onClickAway(event);
-    },
-    [onClickAway, typeof target === 'function' ? undefined : target],
-  );
+    };
 
-  useEffect(() => {
     document.addEventListener(eventName, handler);
 
     return () => {
       document.removeEventListener(eventName, handler);
     };
-  }, [eventName, handler]);
+  }, [eventName]);
 }
