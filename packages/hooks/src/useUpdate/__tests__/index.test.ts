b99fabdc45094cf236822d2e3f0d17476c6e202f
ityuany <519495771@qq.com>
2020-11-20 18:00:01 +0800

    fix：useUpdate
    Prevent the number from increasing too large and exceeding the maximum boundary

---
+++ packages/hooks/src/useUpdate/__tests__/index.test.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import { renderHook, act } from '@testing-library/react-hooks';
+import useUpdate from '..';
+import usePrevious from '../../usePrevious';
+import { times } from 'lodash';
+
+describe('useUpdate', () => {
+  it('should update', () => {
+    const hooks = renderHook(() => {
+      const [count, setCount] = useState(0);
+      const previous = usePrevious(count);
+      const update = useUpdate();
+      useEffect(() => {
+        if (count === previous) {
+          setCount(count + 1);
+        }
+      });
+      return {
+        update,
+        count,
+      };
+    });
+    expect(hooks.result.current.count).toEqual(0);
+    act(hooks.result.current.update);
+    expect(hooks.result.current.count).toEqual(1);
+  });
+  it('should return same update function', () => {
+    const hooks = renderHook(() => useUpdate());
+    const preUpdate = hooks.result.current;
+    hooks.rerender();
+    expect(hooks.result.current).toEqual(preUpdate);
+  });
+});
