1e417b33e0a7e90d47ca180ed6fc1129ba7b7816
卡晨 <guangbo.hgb@antfin.com>
2020-12-04 17:18:34 +0800

    fix: remove comments

--- packages/hooks/src/useSessionStorageState/index.ts
+++ packages/hooks/src/useSessionStorageState/index.ts
@@ -1,18 +1,5 @@
 import { createUseStorageState } from '../createUseStorageState';
 
-// function useSessionStorageState<T = undefined>(
-//   key: string,
-// ): [T | undefined, (value?: T | ((previousState?: T) => T)) => void];
-
-// function useSessionStorageState<T>(
-//   key: string,
-//   defaultValue: T | (() => T),
-// ): [T, (value?: T | ((previousState: T) => T)) => void];
-
-// function useSessionStorageState<T>(key: string, defaultValue?: T | (() => T)) {
-//   return useStorageState(() => sessionStorage, key, defaultValue);
-// }
-
 const useSessionStorageState = createUseStorageState(
   typeof window === 'object' ? window.sessionStorage : null,
 );
