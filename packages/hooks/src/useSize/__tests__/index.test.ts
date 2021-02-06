d758f6f76fcbf4af50028d424a78d0e0a6714746
Turkyden <616287091@qq.com>
2021-02-06 20:22:37 +0800

    fix(usesize): remove the test
    remove the test snippet

--- packages/hooks/src/useSize/__tests__/index.test.ts
+++ packages/hooks/src/useSize/__tests__/index.test.ts
@@ -6,10 +6,4 @@ describe('useSize', () => {
   it('should be defined', () => {
     expect(useSize).toBeDefined();
   });
-  it('with argument', () => {
-    const hook = renderHook(() => useSize(document.body));
-    // with args, the init size should be the real size of the element, in node test env, it's 0 though
-    expect(hook.result.current.width).toEqual(0);
-    expect(hook.result.current.height).toEqual(0);
-  });
 });
