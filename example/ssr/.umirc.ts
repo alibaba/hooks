e0b48c1d362695e526ae1cb47fadb782a4378141
尽龙 <brickspert.fjl@antfin.com>
2021-05-17 16:01:03 +0800

    chore: add ssr example

---
+++ example/ssr/.umirc.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [{ path: '/', component: '@/pages/index' }],
+  ssr: {},
+});
