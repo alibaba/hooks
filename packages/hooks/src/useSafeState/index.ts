ddb832b371a74819e8f2dfd1a655737d2ea374dd
尽龙 <brickspert.fjl@antfin.com>
2021-06-01 16:06:15 +0800

    fix: useSafeState return add useCallback

--- packages/hooks/src/useSafeState/index.ts
+++ packages/hooks/src/useSafeState/index.ts
@@ -1,22 +1,20 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import useUnmountedRef from '../useUnmountedRef';
 
-import React, { Dispatch, SetStateAction } from 'react'
-import useUnmountedRef from '../useUnmountedRef'
-
-function useSafeState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>]
+function useSafeState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>];
 
 function useSafeState<S = undefined>(): [S | undefined, Dispatch<SetStateAction<S | undefined>>];
 
-
 function useSafeState(initialState?) {
-  const unmountedRef = useUnmountedRef()
-  const [state, setState] = React.useState(initialState)
-  const setCurrentState = (currentState) => {
+  const unmountedRef = useUnmountedRef();
+  const [state, setState] = React.useState(initialState);
+  const setCurrentState = React.useCallback((currentState) => {
     /** 如果组件已经卸载则不再更新 state */
-    if (unmountedRef.current) return
-    setState(currentState)
-  }
+    if (unmountedRef.current) return;
+    setState(currentState);
+  }, []);
 
-  return [state, setCurrentState] as const
+  return [state, setCurrentState] as const;
 }
 
-export default useSafeState
+export default useSafeState;
