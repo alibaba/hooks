c2c47d5dbd80e8fdb1313f87ec50bbb16103c299
潇见 <xiaojian.lj@antgroup.com>
2021-06-07 17:33:37 +0800

    chore: 增加 taro 测试环境

---
+++ example/taro/config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    NODE_ENV: '"development"',
+  },
+  defineConstants: {},
+  mini: {},
+  h5: {},
+};
---
+++ example/taro/src/app.config.ts
@@ -0,0 +1,9 @@
+export default {
+  pages: ['pages/index/index'],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black',
+  },
+};
