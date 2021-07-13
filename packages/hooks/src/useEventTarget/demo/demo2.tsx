f0b1ce90b97a6f649212c9181d27745a0760cfd4
尽龙 <brickspert.fjl@antfin.com>
2021-07-13 10:10:33 +0800

    refactor: useEventTarget

--- packages/hooks/src/useEventTarget/demo/demo2.tsx
+++ packages/hooks/src/useEventTarget/demo/demo2.tsx
@@ -3,10 +3,10 @@
  * desc: Controlled Input component with number input only
  *
  * title.zh-CN: 自定义转换函数
- * desc.zh-CN: 只能输入数字的受控Input组件
+ * desc.zh-CN: 只能输入数字的 input 组件
  */
 
-import React, { Fragment } from 'react';
+import React from 'react';
 import { useEventTarget } from 'ahooks';
 
 export default () => {
@@ -15,9 +15,9 @@ export default () => {
   });
 
   return (
-    <Fragment>
+    <div>
       <input
-        value={value || ''}
+        value={value}
         onChange={onChange}
         style={{ width: 200, marginRight: 20 }}
         placeholder="请输入"
@@ -25,6 +25,6 @@ export default () => {
       <button type="button" onClick={reset}>
         reset
       </button>
-    </Fragment>
+    </div>
   );
 };
