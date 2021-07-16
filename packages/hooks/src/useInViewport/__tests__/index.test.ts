771f24462b590fbef26cd3e4a0f2cb218b944e4e
尽龙 <brickspert.fjl@antfin.com>
2021-07-16 15:12:39 +0800

    test: useInViewport test case

--- packages/hooks/src/useInViewport/__tests__/index.test.ts
+++ packages/hooks/src/useInViewport/__tests__/index.test.ts
@@ -1,13 +1,7 @@
-import { renderHook } from '@testing-library/react-hooks';
 import useInViewport from '../index';
 
 describe('useInViewport', () => {
   it('should be defined', () => {
     expect(useInViewport).toBeDefined();
-  });
-
-  it('with argument', () => {
-    const hook = renderHook(() => useInViewport(document.body));
-    expect(hook.result.current).toEqual(false);
   });
 });
