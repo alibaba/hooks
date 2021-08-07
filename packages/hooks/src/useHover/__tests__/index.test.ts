2a393d5757d70ada05bf6dc3abbe76d5b8022940
rayhomie <1572801584@qq.com>
2021-08-07 22:38:56 +0800

    test: add test for useHover

--- packages/hooks/src/useHover/__tests__/index.test.ts
+++
@@ -1,8 +0,0 @@
-// write your test cases here
-import useHover from '../index';
-
-describe('useHover', () => {
-  it('should be defined', () => {
-    expect(useHover).toBeDefined();
-  });
-});
---
+++ packages/hooks/src/useHover/__tests__/index.test.tsx
@@ -0,0 +1,42 @@
+// write your test cases here
+import React from 'react';
+import { renderHook, act } from '@testing-library/react-hooks';
+import { render, fireEvent } from '@testing-library/react';
+import useHover from '../index';
+
+describe('useHover', () => {
+  it('should be defined', () => {
+    expect(useHover).toBeDefined();
+  });
+
+  it('should work', () => {
+    const { getByText } = render(<button>Hover</button>);
+    let trigger = 0;
+    const { result } = renderHook(() =>
+      useHover(getByText('Hover'), {
+        onEnter: () => {
+          trigger++;
+        },
+        onLeave: () => {
+          trigger++;
+        },
+      }),
+    );
+
+    expect(result.current).toBe(false);
+
+    act(() => {
+      fireEvent.mouseOver(getByText('Hover'), () => {
+        expect(result.current).toBe(true);
+        expect(trigger).toBe(1);
+      });
+    });
+
+    act(() => {
+      fireEvent.mouseLeave(getByText('Hover'), () => {
+        expect(result.current).toBe(false);
+        expect(trigger).toBe(2);
+      });
+    });
+  });
+});
