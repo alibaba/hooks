0dfda462bd08b11433f8de8c97f145e593c5debb
rayhomie <1572801584@qq.com>
2021-08-08 10:21:20 +0800

    test: useRafState & useIsomorphicLayoutEffect

--- packages/hooks/src/useRafState/__tests__/index.test.ts
+++ packages/hooks/src/useRafState/__tests__/index.test.ts
@@ -1,7 +1,21 @@
+import { renderHook, act } from '@testing-library/react-hooks';
 import useRafState from '../index';
 
 describe('useRafState', () => {
   it('should be defined', () => {
     expect(useRafState).toBeDefined();
+  });
+
+  it('should work', () => {
+    const { result } = renderHook(() => useRafState(0));
+    const [rafState, setRafState] = result.current;
+    expect(rafState).toBe(0);
+
+    act(() => {
+      setRafState(1);
+      requestAnimationFrame(() => {
+        expect(rafState).toBe(1);
+      });
+    });
   });
 });
