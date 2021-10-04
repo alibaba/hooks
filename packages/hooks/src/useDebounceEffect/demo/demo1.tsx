c38fa4c82a87ea679d9852549cb643ca54d7b349
吴文达 <296190745@qq.com>
2021-10-04 22:42:59 +0800

    修复路径错误导致的文档渲染多个tab

--- packages/hooks/src/useDebounceEffect/demo/demo1.tsx
+++ packages/hooks/src/useDebounceEffect/demo/demo1.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import useDebounceEffect from '../';
+import { useDebounceEffect } from 'ahooks';
 
 export default () => {
   const [value, setValue] = useState('hello');
