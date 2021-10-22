d180a91d18ba2923b13fdd6a53b3ea9cab009eac
tocaata <tuotaka@gmail.com>
2021-10-22 10:49:36 +0800

    Update index.ts
    fnRef is useless. remove it.

--- packages/hooks/src/usePersistFn/index.ts
+++ packages/hooks/src/usePersistFn/index.ts
@@ -3,13 +3,10 @@ import { useRef } from 'react';
 export type noop = (...args: any[]) => any;
 
 function usePersistFn<T extends noop>(fn: T) {
-  const fnRef = useRef<T>(fn);
-  fnRef.current = fn;
-
   const persistFn = useRef<T>();
   if (!persistFn.current) {
     persistFn.current = function (...args) {
-      return fnRef.current!.apply(this, args);
+      return fn!.apply(this, args);
     } as T;
   }
 
