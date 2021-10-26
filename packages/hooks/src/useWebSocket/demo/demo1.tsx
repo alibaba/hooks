c6cf8d2671e86b3ba3529941f93f021d9d3bc467
尽龙 <brickspert.fjl@antfin.com>
2021-10-26 21:20:50 +0800

    ci: fix ci error

--- packages/hooks/src/useWebSocket/demo/demo1.tsx
+++ packages/hooks/src/useWebSocket/demo/demo1.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useMemo } from 'react';
-// import { useWebSocket } from 'ahooks';
-import useWebSocket from '../';
+import { useWebSocket } from 'ahooks';
 
 enum ReadyState {
   Connecting = 0,
