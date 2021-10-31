1d23a66264cb11a12cf1825dc1e899a0732cdcf0
rayhomie <1572801584@qq.com>
2021-10-31 12:59:54 +0800

    test: useRequest mutate & loadingDelay should work

--- packages/hooks/src/useRequest/__tests__/index.test.ts
+++ packages/hooks/src/useRequest/__tests__/index.test.ts
@@ -174,153 +174,45 @@ describe('useRequest', () => {
     hook.unmount();
   });
 
-  // it('useRequest fetchKey should work', async () => {
-  //   act(() => {
-  //     hook = setUp(request, {
-  //       manual: true,
-  //       fetchKey: (id) => id,
-  //     });
-  //   });
-  //   act(() => {
-  //     hook.result.current.run(1);
-  //     hook.result.current.run(2);
-  //     hook.result.current.run(3);
-  //   });
-  //   expect(hook.result.current.fetches[1].loading).toEqual(true);
-  //   expect(hook.result.current.fetches[2].loading).toEqual(true);
-  //   expect(hook.result.current.fetches[3].loading).toEqual(true);
-  //   jest.runAllTimers();
-  //   await hook.waitForNextUpdate();
-  //   expect(hook.result.current.fetches[1].loading).toEqual(false);
-  //   expect(hook.result.current.fetches[2].loading).toEqual(false);
-  //   expect(hook.result.current.fetches[3].loading).toEqual(false);
-  //   expect(hook.result.current.fetches[1].data).toEqual('success');
-  //   expect(hook.result.current.fetches[2].data).toEqual('success');
-  //   expect(hook.result.current.fetches[3].data).toEqual('success');
-  //   hook.unmount();
-  // });
+  it('useRequest mutate should work', async () => {
+    act(() => {
+      hook = setUp(request, {});
+    });
+    jest.runAllTimers();
+    await hook.waitForNextUpdate();
+    expect(hook.result.current.data).toEqual('success');
+    act(() => {
+      hook.result.current.mutate('hello');
+    });
+    expect(hook.result.current.data).toEqual('hello');
+    hook.unmount();
+  });
 
-  // it('useRequest debounceInterval should work', async () => {
-  //   const callback = jest.fn();
+  it('useRequest loadingDelay should work', async () => {
+    act(() => {
+      hook = setUp(request, {
+        loadingDelay: 2000,
+      });
+    });
+    expect(hook.result.current.loading).toEqual(false);
+    jest.runAllTimers();
+    await hook.waitForNextUpdate();
+    expect(hook.result.current.loading).toEqual(false);
+    hook.unmount();
 
-  //   act(() => {
-  //     hook = setUp(
-  //       () => {
-  //         callback();
-  //         return request();
-  //       },
-  //       {
-  //         manual: true,
-  //         debounceInterval: 100,
-  //       },
-  //     );
-  //   });
-
-  //   act(() => {
-  //     hook.result.current.run(1);
-  //     jest.advanceTimersByTime(50);
-  //     hook.result.current.run(2);
-  //     jest.advanceTimersByTime(50);
-  //     hook.result.current.run(3);
-  //     jest.advanceTimersByTime(50);
-  //     hook.result.current.run(4);
-  //   });
-
-  //   jest.runAllTimers();
-  //   await hook.waitForNextUpdate();
-  //   expect(callback).toHaveBeenCalledTimes(1);
-
-  //   act(() => {
-  //     hook.result.current.run(1);
-  //     jest.advanceTimersByTime(50);
-  //     hook.result.current.run(2);
-  //     jest.advanceTimersByTime(50);
-  //     hook.result.current.run(3);
-  //     jest.advanceTimersByTime(50);
-  //     hook.result.current.run(4);
-  //   });
-
-  //   jest.runAllTimers();
-  //   await hook.waitForNextUpdate();
-  //   expect(callback).toHaveBeenCalledTimes(2);
-
-  //   hook.unmount();
-  // });
-
-  // it('useRequest throttleInterval should work', async () => {
-  //   const callback = jest.fn();
-
-  //   act(() => {
-  //     hook = setUp(
-  //       () => {
-  //         callback();
-  //         return request();
-  //       },
-  //       {
-  //         manual: true,
-  //         throttleInterval: 100,
-  //       },
-  //     );
-  //   });
-
-  //   act(() => {
-  //     hook.result.current.run(1);
-  //     jest.advanceTimersByTime(50);
-  //     hook.result.current.run(2);
-  //     jest.advanceTimersByTime(50);
-  //     hook.result.current.run(3);
-  //     jest.advanceTimersByTime(50);
-  //     hook.result.current.run(4);
-  //   });
-
-  //   jest.runAllTimers();
-  //   await hook.waitForNextUpdate();
-  //   expect(callback).toHaveBeenCalledTimes(2);
-
-  //   hook.unmount();
-  // });
-
-  // it('useRequest mutate should work', async () => {
-  //   act(() => {
-  //     hook = setUp(request);
-  //   });
-  //   jest.runAllTimers();
-  //   await hook.waitForNextUpdate();
-  //   expect(hook.result.current.data).toEqual('success');
-  //   act(() => {
-  //     hook.result.current.mutate('hello');
-  //   });
-  //   expect(hook.result.current.data).toEqual('hello');
-  //   hook.unmount();
-  // });
-
-  // it('useRequest loadingDelay should work', async () => {
-  //   act(() => {
-  //     hook = setUp(request, {
-  //       loadingDelay: 2000,
-  //     });
-  //   });
-  //   expect(hook.result.current.loading).toEqual(false);
-  //   jest.runAllTimers();
-  //   await hook.waitForNextUpdate();
-  //   expect(hook.result.current.loading).toEqual(false);
-  //   hook.unmount();
-  // });
-
-  // it('useRequest loadingDelay should delay', async () => {
-  //   act(() => {
-  //     hook = setUp(request, {
-  //       loadingDelay: 500,
-  //     });
-  //   });
-  //   expect(hook.result.current.loading).toEqual(false);
-  //   jest.advanceTimersByTime(501);
-  //   expect(hook.result.current.loading).toEqual(true);
-  //   jest.runAllTimers();
-  //   await hook.waitForNextUpdate();
-  //   expect(hook.result.current.loading).toEqual(false);
-  //   hook.unmount();
-  // });
+    act(() => {
+      hook = setUp(request, {
+        loadingDelay: 500,
+      });
+    });
+    expect(hook.result.current.loading).toEqual(false);
+    jest.advanceTimersByTime(501);
+    expect(hook.result.current.loading).toEqual(true);
+    jest.runAllTimers();
+    await hook.waitForNextUpdate();
+    expect(hook.result.current.loading).toEqual(false);
+    hook.unmount();
+  });
 
   // it('useRequest refreshDeps should work', async () => {
   //   act(() => {
