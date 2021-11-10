a68592d16bc90a7566b2dabd2a2cfa787756b3ff
尽龙 <brickspert.fjl@antfin.com>
2021-11-10 18:13:55 +0800

    chore: upgrade core-js to v3

--- packages/hooks/src/utils/depsAreSame.ts
+++ packages/hooks/src/utils/depsAreSame.ts
@@ -1,19 +1,5 @@
 import type { DependencyList } from 'react';
 
-if (!Object.is) {
-  Object.is = function (x, y) {
-    // SameValue algorithm
-    if (x === y) {
-      // Steps 1-5, 7-10
-      // Steps 6.b-6.e: +0 != -0
-      return x !== 0 || 1 / x === 1 / y;
-    } else {
-      // Step 6.a: NaN == NaN
-      return x !== x && y !== y;
-    }
-  };
-}
-
 export default function depsAreSame(oldDeps: DependencyList, deps: DependencyList): boolean {
   if (oldDeps === deps) return true;
   for (let i = 0; i < oldDeps.length; i++) {
