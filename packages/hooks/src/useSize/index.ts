b65bcfd8ae5cbc2c415778edbe005cdee8dbab1c
CJY <fanluan058@gmail.com>
2021-11-16 19:01:34 +0800

    feat: use getBoundingClientRect to get high precision size

--- packages/hooks/src/useSize/index.ts
+++ packages/hooks/src/useSize/index.ts
@@ -6,6 +6,26 @@ import useIsomorphicLayoutEffectWithTarget from '../utils/useIsomorphicLayoutEff
 
 type Size = { width: number; height: number };
 
+function getTargetSize(target: BasicTarget) {
+  try {
+    const { width, height } = target.getBoundingClientRect();
+
+    return {
+      width,
+      height,
+    };
+  } catch (err) {
+    err;
+    // fallback to clientSize
+    const { clientWidth, clientHeight } = target;
+
+    return {
+      width: clientWidth,
+      height: clientHeight,
+    };
+  }
+}
+
 function useSize(target: BasicTarget): Size | undefined {
   const [state, setState] = useRafState<Size>();
 
@@ -19,11 +39,7 @@ function useSize(target: BasicTarget): Size | undefined {
 
       const resizeObserver = new ResizeObserver((entries) => {
         entries.forEach((entry) => {
-          const { clientWidth, clientHeight } = entry.target;
-          setState({
-            width: clientWidth,
-            height: clientHeight,
-          });
+          setState(getTargetSize(entry.target));
         });
       });
 
