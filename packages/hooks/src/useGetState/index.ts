548cc8970786595e6cffbd8134dcae6f7f010570
CJY <fanluan058@gmail.com>
2021-12-31 10:44:45 +0800

    feat: 使用重载语法优化类型定义、增加用例提示

--- packages/hooks/src/useGetState/index.ts
+++ packages/hooks/src/useGetState/index.ts
@@ -1,15 +1,17 @@
 import { useState, useRef, useCallback, Dispatch, SetStateAction } from 'react';
 
-type GetState<S> = () => S;
+type GetStateAction<S> = () => S;
 
-function useGetState<S = undefined>(initialState?: S | (() => S)): [S, Dispatch<SetStateAction<S>>, GetState<S>] {
-  const [state, setState] = useState<S>(initialState as S);
-  const stateRef = useRef<S>(state);
-  stateRef.current = state;
+function useGetState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>, GetStateAction<S>]
+function useGetState<S = undefined>(): [S | undefined, Dispatch<SetStateAction<S | undefined>>, GetStateAction<S | undefined>]
+function useGetState<S>(initialState?: S) {
+  const [state, setState] = useState<S>(initialState as S)
+  const stateRef = useRef<S>(state)
+  stateRef.current = state
 
-  const getState = useCallback<GetState<S>>(() => stateRef.current, []);
+  const getState = useCallback<GetState<S>>(() => stateRef.current, [])
 
-  return [state, setState, getState];
+  return [state, setState, getState]
 }
 
 export default useGetState;
