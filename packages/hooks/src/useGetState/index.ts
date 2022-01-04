680e3dc8c8d09fd7fd2281a2590aa9d5087f77ca
尽龙 <brickspert.fjl@antfin.com>
2022-01-04 17:39:14 +0800

    typo

--- packages/hooks/src/useGetState/index.ts
+++ packages/hooks/src/useGetState/index.ts
@@ -1,17 +1,24 @@
-import { useState, useRef, useCallback, Dispatch, SetStateAction } from 'react';
+import type { Dispatch, SetStateAction } from 'react';
+import { useState, useRef, useCallback } from 'react';
 
 type GetStateAction<S> = () => S;
 
-function useGetState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>, GetStateAction<S>]
-function useGetState<S = undefined>(): [S | undefined, Dispatch<SetStateAction<S | undefined>>, GetStateAction<S | undefined>]
+function useGetState<S>(
+  initialState: S | (() => S),
+): [S, Dispatch<SetStateAction<S>>, GetStateAction<S>];
+function useGetState<S = undefined>(): [
+  S | undefined,
+  Dispatch<SetStateAction<S | undefined>>,
+  GetStateAction<S | undefined>,
+];
 function useGetState<S>(initialState?: S) {
-  const [state, setState] = useState<S>(initialState as S)
-  const stateRef = useRef<S>(state)
-  stateRef.current = state
+  const [state, setState] = useState(initialState);
+  const stateRef = useRef(state);
+  stateRef.current = state;
 
-  const getState = useCallback<GetStateAction<S>>(() => stateRef.current, [])
+  const getState = useCallback(() => stateRef.current, []);
 
-  return [state, setState, getState]
+  return [state, setState, getState];
 }
 
 export default useGetState;
