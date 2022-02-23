07502494960dda725bb6821ba2c69148c1ca154d
Chuck Fan <fanck0605@qq.com>
2022-02-23 19:53:56 +0800

    fix: useMemoizedFn return a plain function type

--- packages/hooks/src/useMemoizedFn/index.ts
+++ packages/hooks/src/useMemoizedFn/index.ts
@@ -2,6 +2,11 @@ import { useMemo, useRef } from 'react';
 
 type noop = (this: any, ...args: any[]) => any;
 
+type PickFunction<T extends noop> = (
+  this: ThisParameterType<T>,
+  ...args: Parameters<T>
+) => ReturnType<T>;
+
 function useMemoizedFn<T extends noop>(fn: T) {
   if (process.env.NODE_ENV === 'development') {
     if (typeof fn !== 'function') {
@@ -15,11 +20,11 @@ function useMemoizedFn<T extends noop>(fn: T) {
   // https://github.com/alibaba/hooks/issues/728
   fnRef.current = useMemo(() => fn, [fn]);
 
-  const memoizedFn = useRef<T>();
+  const memoizedFn = useRef<PickFunction<T>>();
   if (!memoizedFn.current) {
     memoizedFn.current = function (this, ...args) {
       return fnRef.current.apply(this, args);
-    } as T;
+    };
   }
 
   return memoizedFn.current;
