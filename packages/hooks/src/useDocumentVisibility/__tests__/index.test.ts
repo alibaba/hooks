910eaa7fc7f50eac9c143c07d56bb349407e5269
erweixin <2991552132@qq.com>
2022-04-21 00:20:21 +0800

    test: update useDocumentVisibility test case

--- packages/hooks/src/useDocumentVisibility/__tests__/index.test.ts
+++ packages/hooks/src/useDocumentVisibility/__tests__/index.test.ts
@@ -1,7 +1,46 @@
 import useDocumentVisibility from '../index';
+import { renderHook, act } from '@testing-library/react-hooks';
+
+const mockIsBrowser = jest.fn();
+const mockDocumentVisibilityState = jest.spyOn(document, 'visibilityState', 'get');
+
+jest.mock('../../utils/isBrowser', () => {
+  return {
+    __esModule: true,
+    get default() {
+      return mockIsBrowser();
+    },
+  };
+});
+
+afterAll(() => {
+  jest.clearAllMocks();
+});
 
 describe('useDocumentVisibility', () => {
   it('should be defined', () => {
     expect(useDocumentVisibility).toBeDefined();
+  });
+
+  it('isBrowser effect corrent', async () => {
+    mockDocumentVisibilityState.mockReturnValue('hidden');
+    // Object.defineProperty(document, 'visibilityState', { value: 'hidden', writable: true });
+    mockIsBrowser.mockReturnValue(false);
+    const { result } = renderHook(() => useDocumentVisibility());
+    expect(result.current).toEqual('visible');
+  });
+
+  it('visibilitychange update correct ', async () => {
+    mockDocumentVisibilityState.mockReturnValue('hidden');
+    // Object.defineProperty(document, 'visibilityState', { value: 'hidden', writable: true });
+    mockIsBrowser.mockReturnValue(true);
+    const { result } = renderHook(() => useDocumentVisibility());
+    expect(result.current).toEqual('hidden');
+    mockDocumentVisibilityState.mockReturnValue('visible');
+    // Object.defineProperty(document, 'visibilityState', { value: 'visible', writable: true });
+    act(() => {
+      document.dispatchEvent(new Event('visibilitychange'));
+    });
+    expect(result.current).toEqual('visible');
   });
 });
