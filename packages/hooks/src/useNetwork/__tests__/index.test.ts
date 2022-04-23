758f02af53c028e02116c0d23cac3c42ea8c0355
erweixin <2991552132@qq.com>
2022-04-23 14:21:23 +0800

    test: update useNetwork test case

--- packages/hooks/src/useNetwork/__tests__/index.test.ts
+++ packages/hooks/src/useNetwork/__tests__/index.test.ts
@@ -1,9 +1,21 @@
 import useNetwork from '../index';
-
-// const setUp = () => renderHook(() => useNetwork());
+import { renderHook, act } from '@testing-library/react-hooks';
 
 describe('useNetwork', () => {
   it('should be defined', () => {
     expect(useNetwork).toBeDefined();
+  });
+
+  it('toggle network state', () => {
+    const { result } = renderHook(() => useNetwork());
+    expect(result.current.online).toBeTruthy();
+    act(() => {
+      window.dispatchEvent(new Event('offline'));
+    });
+    expect(result.current.online).toBeFalsy();
+    act(() => {
+      window.dispatchEvent(new Event('online'));
+    });
+    expect(result.current.online).toBeTruthy();
   });
 });
