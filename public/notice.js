d630b37fc609640fbdff0f30ef3183fbc3f755b2
brickspert <576679268@qq.com>
2022-04-29 21:28:34 +0800

    doc: change the v3 domain

--- public/notice.js
+++ public/notice.js
@@ -1,6 +1,6 @@
 let notice = document.createElement('div');
 notice.className = 'notice';
 notice.innerHTML = `<div class="notice-content">
- ahooks 3.0 is comming! welcome to try it https://ahooks-next.surge.sh/
+ ahooks 3.0 is comming! welcome to try it https://ahooks.js.org/
 </div>`;
 document.body.appendChild(notice);
