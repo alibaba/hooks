74c764b4f8e11f54ff74f9f59818589ed13b8673
lijianan <574980606@qq.com>
2022-07-07 23:13:36 +0800

    fix: fix test

--- packages/hooks/src/useCountDown/__tests__/index.test.ts
+++ packages/hooks/src/useCountDown/__tests__/index.test.ts
@@ -135,4 +135,105 @@ describe('useCountDown', () => {
     });
     expect(result.current[0]).toBe(0);
   });
+
+  it('should initialize correctly with undefined leftTime', () => {
+    const { result } = setup();
+
+    const [count, formattedRes] = result.current;
+
+    expect(count).toBe(0);
+    expect(formattedRes).toEqual({
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      milliseconds: 0,
+    });
+  });
+
+  it('should initialize correctly with correct leftTime', () => {
+    const { result } = setup({ leftTime: 5 * 1000, interval: 1000 });
+    const [count, formattedRes] = result.current;
+    expect(count).toBe(5000);
+    expect(formattedRes.seconds).toBe(5);
+    expect(formattedRes.milliseconds).toBe(0);
+  });
+
+  it('should work manually', () => {
+    const { result, rerender } = setup({ interval: 100 });
+
+    rerender({ leftTime: 5 * 1000, interval: 1000 });
+    expect(result.current[0]).toBe(5000);
+    expect(result.current[1].seconds).toBe(5);
+
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+    expect(result.current[0]).toBe(4000);
+    expect(result.current[1].seconds).toBe(4);
+
+    act(() => {
+      jest.advanceTimersByTime(4000);
+    });
+    expect(result.current[0]).toEqual(0);
+    expect(result.current[1].seconds).toBe(0);
+
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+
+    expect(result.current[0]).toEqual(0);
+    expect(result.current[1].seconds).toBe(0);
+  });
+
+  it('should work automatically', () => {
+    const { result } = setup({ leftTime: 5 * 1000, interval: 1000 });
+
+    expect(result.current[0]).toBe(5000);
+    expect(result.current[1].seconds).toBe(5);
+
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+    expect(result.current[0]).toBe(4000);
+    expect(result.current[1].seconds).toBe(4);
+
+    act(() => {
+      jest.advanceTimersByTime(4000);
+    });
+    expect(result.current[0]).toBe(0);
+    expect(result.current[1].seconds).toBe(0);
+  });
+
+  it('should work stop', () => {
+    const { result, rerender } = setup({ leftTime: 5 * 1000, interval: 1000 });
+
+    rerender({ leftTime: 5 * 1000, interval: 1000 });
+    expect(result.current[0]).toBe(5000);
+    expect(result.current[1].seconds).toBe(5);
+
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+    expect(result.current[0]).toBe(4000);
+    expect(result.current[1].seconds).toBe(4);
+
+    rerender({ leftTime: undefined });
+    expect(result.current[0]).toBe(0);
+    expect(result.current[1].seconds).toBe(0);
+  });
+
+  it('it onEnd should work', () => {
+    const onEnd = jest.fn();
+    setup({ leftTime: 5 * 1000, interval: 1000, onEnd });
+    act(() => {
+      jest.advanceTimersByTime(6000);
+    });
+    expect(onEnd).toBeCalled();
+  });
+
+  it('timeLeft should be 0 when leftTime less than current time', () => {
+    const { result } = setup({ leftTime: -5 * 1000 });
+    expect(result.current[0]).toBe(0);
+  });
 });
