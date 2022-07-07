2e4a3fb7c7d612bff6366b6d428984c02832201d
lijianan <574980606@qq.com>
2022-07-07 23:07:57 +0800

    fix: fix test

--- packages/hooks/src/useCountDown/__tests__/index.test.ts
+++ packages/hooks/src/useCountDown/__tests__/index.test.ts
@@ -1,6 +1,6 @@
 import { act, renderHook } from '@testing-library/react-hooks';
-import type { Options } from '../index';
 import useCountDown from '../index';
+import type { Options } from '../index';
 
 const setup = (options: Options = {}) =>
   renderHook((props: Options = options) => useCountDown(props));
@@ -15,7 +15,7 @@ describe('useCountDown', () => {
     jest.useRealTimers();
   });
 
-  it('should initialize correctly with undefined leftTime and targetDate', () => {
+  it('should initialize correctly with undefined targetDate', () => {
     const { result } = setup();
 
     const [count, formattedRes] = result.current;
@@ -41,57 +41,7 @@ describe('useCountDown', () => {
     expect(formattedRes.milliseconds).toBe(0);
   });
 
-  it('should initialize correctly with correct leftTime', () => {
-    const { result } = setup({
-      leftTime: 5 * 1000,
-      interval: 1000,
-    });
-    const [count, formattedRes] = result.current;
-    expect(count).toBe(5000);
-    expect(formattedRes.seconds).toBe(5);
-    expect(formattedRes.milliseconds).toBe(0);
-  });
-
-  it('should work leftTime, and ignored targetDate, If both leftTime and targetDate', () => {
-    const { result } = setup({
-      leftTime: 5 * 1000,
-      targetDate: Date.now() + 10000,
-      interval: 1000,
-    });
-    const [count, formattedRes] = result.current;
-    expect(count).toBe(5000);
-    expect(formattedRes.seconds).toBe(5);
-    expect(formattedRes.milliseconds).toBe(0);
-  });
-
-  it('should work manually leftTime', () => {
-    const { result, rerender } = setup({ interval: 100 });
-
-    rerender({ leftTime: 5 * 1000, interval: 1000 });
-    expect(result.current[0]).toBe(5000);
-    expect(result.current[1].seconds).toBe(5);
-
-    act(() => {
-      jest.advanceTimersByTime(1000);
-    });
-    expect(result.current[0]).toBe(4000);
-    expect(result.current[1].seconds).toBe(4);
-
-    act(() => {
-      jest.advanceTimersByTime(4000);
-    });
-    expect(result.current[0]).toEqual(0);
-    expect(result.current[1].seconds).toBe(0);
-
-    act(() => {
-      jest.advanceTimersByTime(1000);
-    });
-
-    expect(result.current[0]).toEqual(0);
-    expect(result.current[1].seconds).toBe(0);
-  });
-
-  it('should work manually targetDate', () => {
+  it('should work manually', () => {
     const { result, rerender } = setup({ interval: 100 });
 
     rerender({ targetDate: Date.now() + 5000, interval: 1000 });
@@ -118,29 +68,7 @@ describe('useCountDown', () => {
     expect(result.current[1].seconds).toBe(0);
   });
 
-  it('should work automatically leftTime', () => {
-    const { result } = setup({
-      leftTime: 5 * 1000,
-      interval: 1000,
-    });
-
-    expect(result.current[0]).toBe(5000);
-    expect(result.current[1].seconds).toBe(5);
-
-    act(() => {
-      jest.advanceTimersByTime(1000);
-    });
-    expect(result.current[0]).toBe(4000);
-    expect(result.current[1].seconds).toBe(4);
-
-    act(() => {
-      jest.advanceTimersByTime(4000);
-    });
-    expect(result.current[0]).toBe(0);
-    expect(result.current[1].seconds).toBe(0);
-  });
-
-  it('should work automatically targetDate', () => {
+  it('should work automatically', () => {
     const { result } = setup({
       targetDate: Date.now() + 5000,
       interval: 1000,
@@ -162,31 +90,7 @@ describe('useCountDown', () => {
     expect(result.current[1].seconds).toBe(0);
   });
 
-  it('should work stop leftTime', () => {
-    const { result, rerender } = setup({
-      leftTime: 5 * 1000,
-      interval: 1000,
-    });
-
-    rerender({
-      leftTime: 5 * 1000,
-      interval: 1000,
-    });
-    expect(result.current[0]).toBe(5000);
-    expect(result.current[1].seconds).toBe(5);
-
-    act(() => {
-      jest.advanceTimersByTime(1000);
-    });
-    expect(result.current[0]).toBe(4000);
-    expect(result.current[1].seconds).toBe(4);
-
-    rerender({ leftTime: undefined });
-    expect(result.current[0]).toBe(0);
-    expect(result.current[1].seconds).toBe(0);
-  });
-
-  it('should work stop targetDate', () => {
+  it('should work stop', () => {
     const { result, rerender } = setup({
       targetDate: Date.now() + 5000,
       interval: 1000,
@@ -205,36 +109,30 @@ describe('useCountDown', () => {
     expect(result.current[0]).toBe(4000);
     expect(result.current[1].seconds).toBe(4);
 
-    rerender({ targetDate: undefined });
+    rerender({
+      targetDate: undefined,
+    });
     expect(result.current[0]).toBe(0);
     expect(result.current[1].seconds).toBe(0);
   });
 
-  it('it onEnd should work leftTime', () => {
+  it('it onEnd should work', () => {
     const onEnd = jest.fn();
-    setup({ leftTime: 5 * 1000, interval: 1000, onEnd });
+    setup({
+      targetDate: Date.now() + 5000,
+      interval: 1000,
+      onEnd,
+    });
     act(() => {
       jest.advanceTimersByTime(6000);
     });
     expect(onEnd).toBeCalled();
-  });
-
-  it('it onEnd should work targetDate', () => {
-    const onEnd = jest.fn();
-    setup({ targetDate: Date.now() + 5000, interval: 1000, onEnd });
-    act(() => {
-      jest.advanceTimersByTime(6000);
-    });
-    expect(onEnd).toBeCalled();
-  });
-
-  it('timeLeft should be 0 when leftTime less than current time', () => {
-    const { result } = setup({ leftTime: -5 * 1000 });
-    expect(result.current[0]).toBe(0);
   });
 
   it('timeLeft should be 0 when target date less than current time', () => {
-    const { result } = setup({ targetDate: Date.now() - 5000 });
+    const { result } = setup({
+      targetDate: Date.now() - 5000,
+    });
     expect(result.current[0]).toBe(0);
   });
 });
