1e11592d39d69e22cddb600e0e268e3538bfb7ff
“konghai” <574980606@qq.com>
2022-07-12 14:42:50 +0800

    fix: fix useRef type

--- packages/hooks/src/utils/createEffectWithTarget.ts
+++ packages/hooks/src/utils/createEffectWithTarget.ts
@@ -22,7 +22,7 @@ const createEffectWithTarget = (useEffectType: typeof useEffect | typeof useLayo
     const lastElementRef = useRef<(Element | null)[]>([]);
     const lastDepsRef = useRef<DependencyList>([]);
 
-    const unLoadRef = useRef<ReturnType<EffectCallback>>();
+    const unLoadRef = useRef<any>();
 
     useEffectType(() => {
       const targets = Array.isArray(target) ? target : [target];
