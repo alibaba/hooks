dfcf8287cfc96db3070b7af1659f6f9710ec5657
lijianan <574980606@qq.com>
2022-07-26 23:05:25 +0800

    fix: remove params check

--- packages/hooks/src/useMutationObserver/index.ts
+++ packages/hooks/src/useMutationObserver/index.ts
@@ -13,33 +13,6 @@ const useMutationObserver = (
       if (!element) {
         return;
       }
-      const { attributes, characterData, childList, attributeOldValue, characterDataOldValue } =
-        options ?? {};
-      // https://developer.mozilla.org/zh-CN/docs/Web/API/MutationObserver/observe#%E5%BC%82%E5%B8%B8
-      if (!attributes && !characterData && !childList) {
-        if (process.env.NODE_ENV === 'development') {
-          console.error(
-            `[useMutationObserver]: The options object must set at least one of 'attributes', 'characterData', or 'childList' to true.`,
-          );
-        }
-        return;
-      }
-      if (!attributes && attributeOldValue) {
-        if (process.env.NODE_ENV === 'development') {
-          console.error(
-            `[useMutationObserver]: The options object may only set 'attributeOldValue' to true when 'attributes' is true or not present.`,
-          );
-        }
-        return;
-      }
-      if (characterDataOldValue && !characterData) {
-        if (process.env.NODE_ENV === 'development') {
-          console.error(
-            `[useMutationObserver]: The options object may only set 'characterDataOldValue' to true when 'characterData' is true or not present.`,
-          );
-        }
-        return;
-      }
       const observer = new MutationObserver(callback);
       observer.observe(element, options);
       return () => {
@@ -48,7 +21,7 @@ const useMutationObserver = (
         }
       };
     },
-    [],
+    [callback, options],
     target,
   );
 };
