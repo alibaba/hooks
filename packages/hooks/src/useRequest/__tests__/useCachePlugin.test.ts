532ca6daad389f60a518578f3da05e0061708942
lujie <lujie@yuantutech.com>
2022-09-08 11:19:00 +0800

    test(useCachePlugin): refactor test

--- packages/hooks/src/useRequest/__tests__/useCachePlugin.test.ts
+++ packages/hooks/src/useRequest/__tests__/useCachePlugin.test.ts
@@ -1,5 +1,4 @@
 import { act, renderHook } from '@testing-library/react-hooks';
-import MockDate from 'mockdate';
 import useRequest, { clearCache } from '../index';
 import { request } from '../../utils/testingHelpers';
 import 'jest-localstorage-mock';
@@ -7,215 +6,131 @@ import 'jest-localstorage-mock';
 describe('useCachePlugin', () => {
   jest.useFakeTimers();
 
-  const setUp = (service, options) => renderHook((o) => useRequest(service, o || options));
+  const setup = (service, options) => renderHook(() => useRequest(service, options));
 
-  let hook;
-  it('useRequest cacheKey should work', async () => {
-    act(() => {
-      hook = setUp(request, {
-        cacheKey: 'testCacheKey',
-      });
-    });
-
-    act(() => {
+  const testCacheKey = async (options: any) => {
+    const hook = setup(request, options);
+    expect(hook.result.current.loading).toEqual(true);
+    await act(async () => {
       jest.runAllTimers();
     });
-    await hook.waitForNextUpdate();
     expect(hook.result.current.loading).toEqual(false);
     expect(hook.result.current.data).toEqual('success');
     hook.unmount();
+  };
+
+  it('useRequest cacheKey should work', async () => {
+    await testCacheKey({
+      cacheKey: 'testCacheKey',
+    });
 
     jest.advanceTimersByTime(100);
 
-    let hook2;
-    act(() => {
-      hook2 = setUp(request, {
-        cacheKey: 'testCacheKey',
-      });
+    const hook2 = setup(request, {
+      cacheKey: 'testCacheKey',
     });
     expect(hook2.result.current.loading).toEqual(true);
     expect(hook2.result.current.data).toEqual('success');
-
-    act(() => {
+    await act(async () => {
       jest.runAllTimers();
     });
-    await hook2.waitForNextUpdate();
     expect(hook2.result.current.loading).toEqual(false);
-    hook2.unmount();
   });
 
   it('useRequest staleTime should work', async () => {
-    MockDate.set(0);
-
-    act(() => {
-      hook = setUp(request, {
-        cacheKey: 'testStaleTime',
-        staleTime: 3000,
-      });
+    await testCacheKey({
+      cacheKey: 'testStaleTime',
+      staleTime: 3000,
     });
-    expect(hook.result.current.loading).toEqual(true);
 
-    act(() => {
-      jest.runAllTimers();
-    });
-    await hook.waitForNextUpdate();
-    expect(hook.result.current.loading).toEqual(false);
-    expect(hook.result.current.data).toEqual('success');
-    hook.unmount();
-    MockDate.set(1000);
+    jest.advanceTimersByTime(1000);
 
-    let hook2;
-    act(() => {
-      hook2 = setUp(request, {
-        cacheKey: 'testStaleTime',
-        staleTime: 3000,
-      });
+    const hook2 = setup(request, {
+      cacheKey: 'testStaleTime',
+      staleTime: 3000,
     });
-    expect(hook.result.current.loading).toEqual(false);
+    expect(hook2.result.current.loading).toEqual(false);
     expect(hook2.result.current.data).toEqual('success');
     hook2.unmount();
-    MockDate.set(3001);
-    let hook3;
-    act(() => {
-      hook3 = setUp(request, {
-        cacheKey: 'testStaleTime',
-        staleTime: 3000,
-      });
+
+    jest.advanceTimersByTime(3001);
+
+    const hook3 = setup(request, {
+      cacheKey: 'testStaleTime',
+      staleTime: 3000,
     });
     expect(hook3.result.current.loading).toEqual(true);
     expect(hook3.result.current.data).toEqual('success');
 
-    act(() => {
+    await act(async () => {
       jest.runAllTimers();
     });
-    await hook3.waitForNextUpdate();
     expect(hook3.result.current.loading).toEqual(false);
-    hook3.unmount();
   });
 
   it('useRequest cacheTime should work', async () => {
-    act(() => {
-      MockDate.set(0);
-      hook = setUp(request, {
-        cacheKey: 'testCacheTime',
-        cacheTime: 5000,
-      });
-    });
-    expect(hook.result.current.loading).toEqual(true);
-
-    act(() => {
-      jest.runAllTimers();
-    });
-    await hook.waitForNextUpdate();
-    expect(hook.result.current.loading).toEqual(false);
-    expect(hook.result.current.data).toEqual('success');
-    hook.unmount();
-
-    act(() => {
-      MockDate.set(1000);
-      jest.advanceTimersByTime(1000);
+    await testCacheKey({
+      cacheKey: 'testCacheTime',
+      cacheTime: 5000,
     });
 
-    let hook2;
-    act(() => {
-      hook2 = setUp(request, {
-        cacheKey: 'testCacheTime',
-        cacheTime: 5000,
-      });
+    jest.advanceTimersByTime(1000);
+
+    const hook2 = setup(request, {
+      cacheKey: 'testCacheTime',
+      cacheTime: 5000,
     });
     expect(hook2.result.current.loading).toEqual(true);
     expect(hook2.result.current.data).toEqual('success');
     hook2.unmount();
-    act(() => {
-      MockDate.set(6001);
-      jest.advanceTimersByTime(5001);
-    });
 
-    let hook3;
-    act(() => {
-      hook3 = setUp(request, {
-        cacheKey: 'testCacheTime',
-        cacheTime: 5000,
-      });
+    jest.advanceTimersByTime(5001);
+
+    const hook3 = setup(request, {
+      cacheKey: 'testCacheTime',
+      cacheTime: 5000,
     });
     expect(hook3.result.current.loading).toEqual(true);
     expect(hook3.result.current.data).toEqual(undefined);
 
-    act(() => {
+    await act(async () => {
       jest.runAllTimers();
     });
-    await hook3.waitForNextUpdate();
     expect(hook3.result.current.loading).toEqual(false);
     expect(hook3.result.current.data).toEqual('success');
-    hook3.unmount();
   });
 
   it('clearCache should work', async () => {
-    MockDate.set(0);
-    act(() => {
-      hook = setUp(request, {
-        cacheKey: 'testClearCache',
-      });
-    });
+    await testCacheKey('testClearCache');
 
-    act(() => {
-      jest.runAllTimers();
-    });
-    await hook.waitForNextUpdate();
-    expect(hook.result.current.loading).toEqual(false);
-    expect(hook.result.current.data).toEqual('success');
-    hook.unmount();
-
-    let hook2;
-    act(() => {
-      clearCache('testClearCache');
-      hook2 = setUp(request, {
-        cacheKey: 'testClearCache',
-      });
+    clearCache('testClearCache');
+    const hook2 = setup(request, {
+      cacheKey: 'testClearCache',
     });
     expect(hook2.result.current.loading).toEqual(true);
     expect(hook2.result.current.data).toEqual(undefined);
-    hook2.unmount();
   });
 
   it('setCache/getCache should work', async () => {
-    MockDate.set(0);
-
     const cacheKey = `setCacheKey`;
-
-    act(() => {
-      hook = setUp(request, {
-        cacheKey,
-        setCache: (data) => localStorage.setItem(cacheKey, JSON.stringify(data)),
-        getCache: () => JSON.parse(localStorage.getItem(cacheKey) || '{}'),
-      });
+    await testCacheKey({
+      cacheKey,
+      setCache: (data) => localStorage.setItem(cacheKey, JSON.stringify(data)),
+      getCache: () => JSON.parse(localStorage.getItem(cacheKey) || '{}'),
     });
 
-    act(() => {
-      jest.runAllTimers();
-    });
-    await hook.waitForNextUpdate();
-    expect(hook.result.current.loading).toEqual(false);
-    expect(hook.result.current.data).toEqual('success');
-    hook.unmount();
-
-    let hook2;
-    act(() => {
-      MockDate.set(1000);
-      hook2 = setUp(request, {
-        cacheKey,
-        setCache: (data) => localStorage.setItem(cacheKey, JSON.stringify(data)),
-        getCache: () => JSON.parse(localStorage.getItem(cacheKey) || '{}'),
-      });
+    jest.advanceTimersByTime(1000);
+    const hook2 = setup(request, {
+      cacheKey,
+      setCache: (data) => localStorage.setItem(cacheKey, JSON.stringify(data)),
+      getCache: () => JSON.parse(localStorage.getItem(cacheKey) || '{}'),
     });
     expect(hook2.result.current.loading).toEqual(true);
     expect(hook2.result.current.data).toEqual('success');
 
-    act(() => {
+    await act(async () => {
       jest.runAllTimers();
     });
-    await hook2.waitForNextUpdate();
     expect(hook2.result.current.loading).toEqual(false);
-    hook2.unmount();
   });
 });
