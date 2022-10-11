0b9f0f629090eec096ecb48c3f8dada05a6676e2
food-billboard <50391608+food-billboard@users.noreply.github.com>
2022-10-11 10:34:57 +0800

    chore(): :white_check_mark: useHash

--- packages/hooks/src/useHash/__tests__/index.test.tsx
+++ packages/hooks/src/useHash/__tests__/index.test.tsx
@@ -3,10 +3,21 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import useHash from '../index';
 
 describe('useHash', () => {
-  it('should hash change', () => {
+  it('should hash change', async () => {
     const hash = '#/newhash';
     const setHash = '#/sethash';
-    const { result } = renderHook(() => useHash());
+    let trigger = 0;
+    let hashHistory = '';
+    let prevHashHistory = '';
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useHash({
+        onChange: (currentHash, prevHash) => {
+          trigger++;
+          hashHistory = currentHash;
+          prevHashHistory = prevHash;
+        },
+      }),
+    );
 
     expect(result.current[0]).toBe('');
 
@@ -14,12 +25,24 @@ describe('useHash', () => {
       window.location.hash = hash;
     });
 
+    await waitForNextUpdate();
+
     expect(result.current[0]).toBe(hash);
+
+    expect(trigger).toBe(1);
+    expect(hashHistory).toBe(hash);
+    expect(prevHashHistory).toBe('');
 
     act(() => {
       result.current[1](setHash);
     });
 
+    await waitForNextUpdate();
+
     expect(result.current[0]).toBe(setHash);
+
+    expect(trigger).toBe(2);
+    expect(hashHistory).toBe(setHash);
+    expect(prevHashHistory).toBe(hash);
   });
 });
