c28806644bdfab7d30aece7cde6ee520888622fd
huangcheng <huangcheng.lq@bytedance.com>
2022-12-07 16:13:43 +0800

    fix: handleResize never perform in React 18 strictMode

--- packages/hooks/src/useResponsive/index.ts
+++ packages/hooks/src/useResponsive/index.ts
@@ -61,9 +61,16 @@ export function useResponsive() {
   useEffect(() => {
     if (!isBrowser) return;
 
+    // In React 18's StrictMode, useEffect perform twice, resize listener is remove, so handleResize is never perform.
+    // https://github.com/alibaba/hooks/issues/1910
+    if (!listening) {
+      window.addEventListener('resize', handleResize);
+    }
+
     const subscriber = () => {
       setState(info);
     };
+
     subscribers.add(subscriber);
     return () => {
       subscribers.delete(subscriber);
@@ -73,20 +80,6 @@ export function useResponsive() {
       }
     };
   }, []);
-
-  // In React 18's StrictMode, useEffect perform twice, resize listener is remove, so handleResize is never perform.
-  // https://github.com/alibaba/hooks/issues/1910
-  useEffect(() => {
-    if (!listening) {
-      window.addEventListener('resize', handleResize);
-    }
-    return () => {
-      if (listening) {
-        window.removeEventListener('resize', handleResize);
-        listening = false;
-      }
-    };
-  }, [listening]);
 
   return state;
 }
