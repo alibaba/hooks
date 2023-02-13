7e642537161639c38a9ed33fa15c829fe513b888
zorn <312073339@qq.com>
2023-02-13 18:16:48 +0800

    refactor(useMemoizedFn): reduce hook usage

--- packages/hooks/src/useMemoizedFn/index.ts
+++ packages/hooks/src/useMemoizedFn/index.ts
@@ -4,32 +4,22 @@ import isDev from '../utils/isDev';
 
 type noop = (this: any, ...args: any[]) => any;
 
-type PickFunction<T extends noop> = (
-  this: ThisParameterType<T>,
-  ...args: Parameters<T>
-) => ReturnType<T>;
-
 function useMemoizedFn<T extends noop>(fn: T) {
-  if (isDev) {
-    if (!isFunction(fn)) {
-      console.error(`useMemoizedFn expected parameter is a function, got ${typeof fn}`);
-    }
+  if (isDev && !isFunction(fn)) {
+    console.error(`useMemoizedFn expected parameter is a function, got ${typeof fn}`);
   }
 
-  const fnRef = useRef<T>(fn);
-
-  // why not write `fnRef.current = fn`?
+  const fnRef = useRef({
+    originalFn: fn,
+    memoizedFn: function (this: ThisParameterType<T>, ...args: Parameters<T>): ReturnType<T> {
+      return fnRef.current.originalFn.apply(this, args);
+    },
+  });
+  // why not write `fnRef.current.originalFn = fn`?
   // https://github.com/alibaba/hooks/issues/728
-  fnRef.current = useMemo(() => fn, [fn]);
+  fnRef.current.originalFn = useMemo(() => fn, [fn]);
 
-  const memoizedFn = useRef<PickFunction<T>>();
-  if (!memoizedFn.current) {
-    memoizedFn.current = function (this, ...args) {
-      return fnRef.current.apply(this, args);
-    };
-  }
-
-  return memoizedFn.current as T;
+  return fnRef.current.memoizedFn as T;
 }
 
 export default useMemoizedFn;
