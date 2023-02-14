2747cda59f357e5a47920296b59af91acb401eac
liuyibo <liuyibo@xylink.com>
2023-02-15 02:07:05 +0800

    test: 优化测试代码逻辑, 补充两个单测

--- packages/hooks/src/useBounding/__tests__/index.test.ts
+++ packages/hooks/src/useBounding/__tests__/index.test.ts
@@ -1,87 +1,114 @@
 import { act, renderHook } from '@testing-library/react';
-import ResizeObserver from 'resize-observer-polyfill';
+import type { RenderHookResult } from '@testing-library/react';
 import useBounding, { INIT_BOUNDING_RECT } from '../index';
-
-window.ResizeObserver = ResizeObserver;
-
-const windowListenerMock: any = {};
-const getBoundingClientRectMock = jest.spyOn(HTMLElement.prototype, 'getBoundingClientRect');
-jest
-  .spyOn(window, 'addEventListener')
-  .mockImplementation(
-    jest.fn((event: any, callback: any) => (windowListenerMock[event] = callback)),
-  );
-const INIT_VALUE = {
-  width: 200,
-  height: 200,
-  left: 100,
-  right: 500,
-  top: 100,
-  bottom: 500,
-  x: 100,
-  y: 100,
-} as DOMRect;
+import type { Target, UseBoundingOptions, UseBoundingRect } from '../index';
 
 describe('useBounding', () => {
-  it('should work with target', async () => {
-    const hook = renderHook(() => useBounding(document.body));
-    expect(hook.result.current).toEqual(INIT_BOUNDING_RECT);
-  });
+  const INIT_VALUE = {
+    width: 200,
+    height: 200,
+    left: 100,
+    right: 500,
+    top: 100,
+    bottom: 500,
+    x: 100,
+    y: 100,
+  } as DOMRect;
 
-  it('should return initial values when target is null', () => {
-    const hook = renderHook(() => useBounding(null));
-    expect(hook.result.current).toEqual(INIT_BOUNDING_RECT);
-  });
+  let windowAddEventListenerMock: any = {};
+  let resizeObserverMock: jest.Mock<any, any, any>;
+  let target: HTMLTextAreaElement;
+  let targetRectMock: jest.SpyInstance<DOMRect, [], any>;
+  let hook: RenderHookResult<UseBoundingRect, unknown>;
 
-  it('should work when window is resizing', async () => {
-    getBoundingClientRectMock.mockReturnValue(INIT_VALUE);
-    const target = document.createElement('div');
-    const hook = renderHook(() => useBounding(target));
-    expect(hook.result.current).toEqual(INIT_VALUE);
+  beforeEach(() => {
+    jest.spyOn(window, 'addEventListener').mockImplementation(
+      jest.fn((event, callback) => {
+        windowAddEventListenerMock[event] = callback;
+      }),
+    );
 
-    getBoundingClientRectMock.mockReturnValue({ ...INIT_VALUE, width: 100, height: 100 });
-    // `useBounding` will not update before `window.onresize` triggers.
-    expect(hook.result.current).toEqual({ ...INIT_VALUE });
-    act(() => windowListenerMock.resize());
-    // `useBounding` will update after `window.onresize` triggers.
-    expect(hook.result.current).toEqual({ ...INIT_VALUE, width: 100, height: 100 });
-    getBoundingClientRectMock.mockClear();
-  });
-
-  it('should work when window is scrolling', () => {
-    getBoundingClientRectMock.mockReturnValue(INIT_VALUE);
-    const target = document.createElement('div');
-    const hook = renderHook(() => useBounding(target));
-    expect(hook.result.current).toEqual(INIT_VALUE);
-
-    getBoundingClientRectMock.mockReturnValue({ ...INIT_VALUE, width: 100, height: 100 });
-    // `useBounding` will not update before `window.onscroll` triggers.
-    expect(hook.result.current).toEqual({ ...INIT_VALUE });
-    act(() => windowListenerMock.scroll());
-    // `useBounding` will update after `window.onscroll` triggers.
-    expect(hook.result.current).toEqual({ ...INIT_VALUE, width: 100, height: 100 });
-    getBoundingClientRectMock.mockClear();
-  });
-
-  it('should disconnect when unmount', async () => {
-    const resizeObserverMock = jest.fn().mockReturnValue({
+    window.ResizeObserver = resizeObserverMock = jest.fn().mockReturnValue({
       observe: () => null,
       disconnect: () => null,
     });
+
+    target = document.createElement('textarea');
+    targetRectMock = jest.spyOn(target, 'getBoundingClientRect').mockReturnValue(INIT_VALUE);
+  });
+
+  afterEach(() => {
+    windowAddEventListenerMock = {};
+    resizeObserverMock.mockRestore();
+    targetRectMock.mockRestore();
+    hook.unmount();
+  });
+
+  function triggerResize() {
+    // `ResizeObserver` API will call once on component mounted
+    const calls = resizeObserverMock.mock.calls;
+    const [resizeObserverCallback] = calls[calls.length - 1];
+
+    act(() => resizeObserverCallback());
+  }
+
+  function setup(el: Target, options?: UseBoundingOptions) {
+    const res = renderHook(() => useBounding(el, options));
+
+    if (el) triggerResize();
+
+    return res;
+  }
+
+  it('should work with target', async () => {
+    hook = setup(target);
+    expect(hook.result.current).toEqual(INIT_VALUE);
+  });
+
+  it('should return initial values when target is null', () => {
+    hook = setup(null);
+    expect(hook.result.current).toEqual(INIT_BOUNDING_RECT);
+  });
+
+  it('should work when the size of target changes', () => {
+    hook = setup(target);
+    // Simulate the size change of the target
+    targetRectMock.mockReturnValue({ ...INIT_VALUE, width: 10, height: 10 });
+    triggerResize();
+    expect(hook.result.current).toEqual({ ...INIT_VALUE, width: 10, height: 10 });
+  });
+
+  it('should reset when unmount', async () => {
+    hook = setup(target);
+    expect(hook.result.current).toEqual(INIT_VALUE);
+    hook.unmount();
+    // TODO: 卸载后拿不到 hook 重置后的值
+    expect(hook.result.current).toEqual(INIT_BOUNDING_RECT);
+  });
+
+  it('should work when the size of window changes', async () => {
+    hook = setup(target);
+    targetRectMock.mockReturnValue({ ...INIT_VALUE, width: 10, height: 10 });
+    act(() => windowAddEventListenerMock.resize());
+    expect(hook.result.current).toEqual({ ...INIT_VALUE, width: 10, height: 10 });
+  });
+
+  it('should work when the window scrolls', () => {
+    hook = setup(target);
+    targetRectMock.mockReturnValue({ ...INIT_VALUE, width: 10, height: 10 });
+    act(() => windowAddEventListenerMock.scroll());
+    expect(hook.result.current).toEqual({ ...INIT_VALUE, width: 10, height: 10 });
+  });
+
+  it('should disconnect when unmount', async () => {
     const disconnect = jest.fn();
+
     resizeObserverMock.mockReturnValue({
       observe: () => null,
       disconnect,
     });
-    window.ResizeObserver = resizeObserverMock;
-
-    const target = document.createElement('div');
-    const hook = renderHook(() => useBounding(target));
+    hook = setup(target);
     hook.unmount();
     expect(disconnect).toBeCalled();
-
-    resizeObserverMock.mockClear();
-    disconnect.mockClear();
-    window.ResizeObserver = ResizeObserver;
   });
 });
