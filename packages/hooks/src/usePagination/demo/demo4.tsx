bc19d0f580ac83144daaabf4cf61951b0ae27776
栗嘉男 <574980606@qq.com>
2023-02-15 10:15:46 +0800

    fix: revert

--- packages/hooks/src/usePagination/demo/demo4.tsx
+++ packages/hooks/src/usePagination/demo/demo4.tsx
@@ -39,7 +39,22 @@ async function getUserList(params: {
   });
 }
 
-const PaginationComponent: React.FC = () => {
+export default () => {
+  const [state, { toggle }] = useBoolean();
+  return (
+    <div>
+      <p>You can click the button multiple times, the conditions of pagination will be cached.</p>
+      <p>
+        <button type="button" onClick={() => toggle()}>
+          show/hide
+        </button>
+      </p>
+      {state && <PaginationComponent />}
+    </div>
+  );
+};
+
+const PaginationComponent = () => {
   const { data, loading, pagination, run, params } = usePagination(
     ({ current, pageSize }, g: string) => {
       return getUserList({
@@ -97,21 +112,6 @@ const PaginationComponent: React.FC = () => {
         showSizeChanger
         style={{ marginTop: 16, textAlign: 'right' }}
       />
-    </div>
-  );
-};
-
-export default () => {
-  const [state, { toggle }] = useBoolean();
-  return (
-    <div>
-      <p>You can click the button multiple times, the conditions of pagination will be cached.</p>
-      <p>
-        <button type="button" onClick={() => toggle()}>
-          show/hide
-        </button>
-      </p>
-      {state && <PaginationComponent />}
     </div>
   );
 };
