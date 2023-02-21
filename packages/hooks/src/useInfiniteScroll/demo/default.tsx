a9a08e3393c8abaa6fd5c1344973307ffe6decdf
xincan <1600083715@qq.com>
2023-02-21 12:31:38 +0800

    docs: 更通俗些的滚动无限加载

--- packages/hooks/src/useInfiniteScroll/demo/default.tsx
+++ packages/hooks/src/useInfiniteScroll/demo/default.tsx
@@ -1,44 +1,58 @@
-import React from 'react';
-import { useInfiniteScroll } from 'ahooks';
+import React, { useRef } from "react"
+import { useInfiniteScroll } from "ahooks"
+import { Data } from "ahooks/lib/useInfiniteScroll/types"
 
 interface Result {
-  list: string[];
-  nextId: string | undefined;
+  list: number[]
 }
 
-const resultData = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'];
+function getLoadMoreList(d: Data = { list: [] }): Promise<Result> {
+  const newD = new Array(10)
+    .fill(d.list.length)
+    .map((item, index) => item + index + 1)
 
-function getLoadMoreList(nextId: string | undefined, limit: number): Promise<Result> {
-  let start = 0;
-  if (nextId) {
-    start = resultData.findIndex((i) => i === nextId);
-  }
-  const end = start + limit;
-  const list = resultData.slice(start, end);
-  const nId = resultData.length >= end ? resultData[end] : undefined;
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({
-        list,
-        nextId: nId,
-      });
-    }, 1000);
-  });
+        list: newD,
+      })
+    }, 1000)
+  })
 }
 
+const isOverflow = (d?: Data, max = 100) =>
+  d?.list === undefined ? true : d.list.length >= max
+
 export default () => {
-  const { data, loading, loadMore, loadingMore } = useInfiniteScroll((d) =>
-    getLoadMoreList(d?.nextId, 4),
-  );
+  const ref = useRef<HTMLDivElement>(null)
+
+  const { data, loading, loadMore, loadingMore, noMore } = useInfiniteScroll(
+    (d) => getLoadMoreList(d),
+    {
+      target: ref,
+      isNoMore: isOverflow,
+    }
+  )
 
   return (
-    <div>
+    <div
+      ref={ref}
+      style={{
+        height: 150,
+        overflow: "auto",
+        border: "1px solid",
+        padding: 12,
+      }}
+    >
       {loading ? (
         <p>loading</p>
       ) : (
         <div>
           {data?.list?.map((item) => (
-            <div key={item} style={{ padding: 12, border: '1px solid #f5f5f5' }}>
+            <div
+              key={item}
+              style={{ padding: 12, border: "1px solid #f5f5f5" }}
+            >
               item-{item}
             </div>
           ))}
@@ -46,14 +60,14 @@ export default () => {
       )}
 
       <div style={{ marginTop: 8 }}>
-        {data?.nextId && (
+        {!noMore && (
           <button type="button" onClick={loadMore} disabled={loadingMore}>
-            {loadingMore ? 'Loading more...' : 'Click to load more'}
+            {loadingMore ? "Loading more..." : "Click to load more"}
           </button>
         )}
 
-        {!data?.nextId && <span>No more data</span>}
+        {noMore && <span>No more data</span>}
       </div>
     </div>
-  );
-};
+  )
+}
