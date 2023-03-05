4b9ea8b43260f95c8d41b3514bb0f8e2e23c2092
thinkasany <480968828@qq.com>
2023-03-05 23:52:13 +0800

    docs: optimize docs indentation

--- packages/hooks/src/useSize/demo/demo2.tsx
+++ packages/hooks/src/useSize/demo/demo2.tsx
@@ -1,9 +1,9 @@
 /**
  * title: pass in the DOM element
- * desc: useSize can receive a dom element as parameter. In SSR scenarios, you can pass in function `()=>dom`
+ * desc: useSize can receive a dom element as parameter. In SSR scenarios, you can pass in function `() => dom`
  *
  * title.zh-CN: 传入 DOM 元素
- * desc.zh-CN: useSize 可以接收 dom，在 SSR 场景可以传入函数 `()=> dom`
+ * desc.zh-CN: useSize 可以接收 dom，在 SSR 场景可以传入函数 `() => dom`
  */
 
 import React from 'react';
