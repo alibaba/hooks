b9c3fa681e5cb0e7010aadde208a19641e7e0a57
栗嘉男 <574980606@qq.com>
2023-03-07 11:02:14 +0800

    style: optimize useTimeout & useInterval

--- packages/hooks/src/useInterval/index.ts
+++ packages/hooks/src/useInterval/index.ts
@@ -2,32 +2,9 @@ import { useCallback, useEffect, useRef } from 'react';
 import useMemoizedFn from '../useMemoizedFn';
 import { isNumber } from '../utils';
 
-function useInterval(
-  fn: () => void,
-  delay: number | undefined,
-  options: {
-    immediate?: boolean;
-  } = {},
-) {
-  const { immediate } = options;
-
+const useInterval = (fn: () => void, delay?: number, options: { immediate?: boolean } = {}) => {
   const timerCallback = useMemoizedFn(fn);
   const timerRef = useRef<NodeJS.Timer | null>(null);
-
-  useEffect(() => {
-    if (!isNumber(delay) || delay < 0) {
-      return;
-    }
-    if (immediate) {
-      timerCallback();
-    }
-    timerRef.current = setInterval(timerCallback, delay);
-    return () => {
-      if (timerRef.current) {
-        clearInterval(timerRef.current);
-      }
-    };
-  }, [delay, timerCallback]);
 
   const clear = useCallback(() => {
     if (timerRef.current) {
@@ -35,7 +12,18 @@ function useInterval(
     }
   }, []);
 
+  useEffect(() => {
+    if (!isNumber(delay) || delay < 0) {
+      return;
+    }
+    if (options.immediate) {
+      timerCallback();
+    }
+    timerRef.current = setInterval(timerCallback, delay);
+    return clear;
+  }, [delay]);
+
   return clear;
-}
+};
 
 export default useInterval;
--- packages/hooks/src/useTimeout/index.ts
+++ packages/hooks/src/useTimeout/index.ts
@@ -2,21 +2,9 @@ import { useCallback, useEffect, useRef } from 'react';
 import useMemoizedFn from '../useMemoizedFn';
 import { isNumber } from '../utils';
 
-function useTimeout(fn: () => void, delay: number | undefined) {
+const useTimeout = (fn: () => void, delay?: number) => {
   const timerCallback = useMemoizedFn(fn);
   const timerRef = useRef<NodeJS.Timer | null>(null);
-
-  useEffect(() => {
-    if (!isNumber(delay) || delay < 0) {
-      return;
-    }
-    timerRef.current = setTimeout(timerCallback, delay);
-    return () => {
-      if (timerRef.current) {
-        clearTimeout(timerRef.current);
-      }
-    };
-  }, [delay]);
 
   const clear = useCallback(() => {
     if (timerRef.current) {
@@ -24,7 +12,15 @@ function useTimeout(fn: () => void, delay: number | undefined) {
     }
   }, []);
 
+  useEffect(() => {
+    if (!isNumber(delay) || delay < 0) {
+      return;
+    }
+    timerRef.current = setTimeout(timerCallback, delay);
+    return clear;
+  }, [delay]);
+
   return clear;
-}
+};
 
 export default useTimeout;
