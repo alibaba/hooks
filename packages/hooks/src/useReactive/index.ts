4f293c3800fdf1f427263b26b7277a838959cc12
云泥 <1656081615@qq.com>
2023-04-07 10:01:06 +0800

    chore: optimize import to reduce bundle size (#2144)

--- packages/hooks/src/useReactive/index.ts
+++ packages/hooks/src/useReactive/index.ts
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import { isPlainObject } from 'lodash';
+import isPlainObject from 'lodash/isPlainObject';
 import useCreation from '../useCreation';
 import useUpdate from '../useUpdate';
 
