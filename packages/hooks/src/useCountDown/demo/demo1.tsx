a6a4ef406e449e624e5c147831664aff0da8c45c
lijianan <574980606@qq.com>
2023-04-11 19:48:34 +0800

    Update demo1.tsx

--- packages/hooks/src/useCountDown/demo/demo1.tsx
+++ packages/hooks/src/useCountDown/demo/demo1.tsx
@@ -11,14 +11,14 @@ import { useCountDown } from 'ahooks';
 
 export default () => {
   const [countdown, formattedRes] = useCountDown({
-    targetDate: '2023-12-31 23:59:59',
+    targetDate: `${new Date().getFullYear()}-12-31 23:59:59`,
   });
   const { days, hours, minutes, seconds, milliseconds } = formattedRes;
 
   return (
     <p>
       There are {days} days {hours} hours {minutes} minutes {seconds} seconds {milliseconds}{' '}
-      milliseconds until 2023-12-31 23:59:59
+      milliseconds until {new Date().getFullYear()}-12-31 23:59:59
     </p>
   );
 };
