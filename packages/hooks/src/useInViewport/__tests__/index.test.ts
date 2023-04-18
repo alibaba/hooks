beaadbd4d85d079294eda098987c3e72bed1a5b2
coderluojz <929747115@qq.com>
2023-04-18 17:47:43 +0800

    feat: add callback and target array test case

--- packages/hooks/src/useInViewport/__tests__/index.test.ts
+++ packages/hooks/src/useInViewport/__tests__/index.test.ts
@@ -1,15 +1,23 @@
+import { act, renderHook } from '@testing-library/react';
 import useInViewport from '../index';
-import { renderHook, act } from '@testing-library/react';
 
 const targetEl = document.createElement('div');
 document.body.appendChild(targetEl);
 
+const observe = jest.fn();
+const disconnect = jest.fn();
+
 const mockIntersectionObserver = jest.fn().mockReturnValue({
-  observe: () => null,
-  disconnect: () => null,
+  observe,
+  disconnect,
 });
 
 window.IntersectionObserver = mockIntersectionObserver;
+
+let isInter = null;
+const callback = ({ isIntersecting }) => {
+  isInter = isIntersecting;
+};
 
 describe('useInViewport', () => {
   it('should work when target is in viewport', async () => {
@@ -30,6 +38,49 @@ describe('useInViewport', () => {
     const [inViewport, ratio] = result.current;
     expect(inViewport).toBeTruthy();
     expect(ratio).toBe(0.5);
+  });
+
+  it('should work when target array is in viewport and has a callback', async () => {
+    const targetEls: HTMLDivElement[] = [];
+    const count = 5;
+    for (let i = 0; i < count; i++) {
+      const target = document.createElement('div');
+      document.body.appendChild(target);
+      targetEls.push(target);
+    }
+
+    renderHook(() => useInViewport(targetEls, { callback }));
+    const calls = mockIntersectionObserver.mock.calls;
+    const [onChange] = calls[calls.length - 1];
+
+    act(() => {
+      onChange([
+        {
+          isIntersecting: null,
+        },
+      ]);
+    });
+    expect(isInter).toBeNull();
+
+    act(() => {
+      onChange([
+        {
+          isIntersecting: true,
+        },
+      ]);
+    });
+    expect(isInter).toBeTruthy();
+
+    act(() => {
+      onChange([
+        {
+          isIntersecting: false,
+        },
+      ]);
+    });
+    expect(isInter).toBeFalsy();
+
+    expect(observe).toHaveBeenCalledTimes(5);
   });
 
   it('should not work when target is null', async () => {
