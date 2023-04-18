ffa8c29697c5136bbf347581519969e7f1fec82c
coderluojz <929747115@qq.com>
2023-04-18 19:39:20 +0800

    fix: test case code optimization

--- packages/hooks/src/useInViewport/__tests__/index.test.ts
+++ packages/hooks/src/useInViewport/__tests__/index.test.ts
@@ -14,11 +14,6 @@ const mockIntersectionObserver = jest.fn().mockReturnValue({
 
 window.IntersectionObserver = mockIntersectionObserver;
 
-let isInter = null;
-const callback = ({ isIntersecting }) => {
-  isInter = isIntersecting;
-};
-
 describe('useInViewport', () => {
   it('should work when target is in viewport', async () => {
     const { result } = renderHook(() => useInViewport(targetEl));
@@ -42,45 +37,30 @@ describe('useInViewport', () => {
 
   it('should work when target array is in viewport and has a callback', async () => {
     const targetEls: HTMLDivElement[] = [];
-    const count = 5;
-    for (let i = 0; i < count; i++) {
+    const callback = jest.fn();
+    for (let i = 0; i < 2; i++) {
       const target = document.createElement('div');
       document.body.appendChild(target);
       targetEls.push(target);
     }
 
-    renderHook(() => useInViewport(targetEls, { callback }));
+    const getValue = (isIntersecting, intersectionRatio) => ({ isIntersecting, intersectionRatio });
+
+    const { result } = renderHook(() => useInViewport(targetEls, { callback }));
     const calls = mockIntersectionObserver.mock.calls;
-    const [onChange] = calls[calls.length - 1];
+    const [observerCallback] = calls[calls.length - 1];
 
-    act(() => {
-      onChange([
-        {
-          isIntersecting: null,
-        },
-      ]);
-    });
-    expect(isInter).toBeNull();
+    const target = getValue(false, 0);
+    act(() => observerCallback([target]));
+    expect(callback).toHaveBeenCalledWith(target);
+    expect(result.current[0]).toBe(false);
+    expect(result.current[1]).toBe(0);
 
-    act(() => {
-      onChange([
-        {
-          isIntersecting: true,
-        },
-      ]);
-    });
-    expect(isInter).toBeTruthy();
-
-    act(() => {
-      onChange([
-        {
-          isIntersecting: false,
-        },
-      ]);
-    });
-    expect(isInter).toBeFalsy();
-
-    expect(observe).toHaveBeenCalledTimes(5);
+    const target1 = getValue(true, 0.5);
+    act(() => observerCallback([target1]));
+    expect(callback).toHaveBeenCalledWith(target1);
+    expect(result.current[0]).toBe(true);
+    expect(result.current[1]).toBe(0.5);
   });
 
   it('should not work when target is null', async () => {
