fc72bfb01aab8219d28dc2f3e7787323c2055069
liuyib <1656081615@qq.com>
2023-04-19 22:14:19 +0800

    chore: update webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -12,8 +12,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         // https://github.com/alibaba/hooks/issues/2155
-        exclude: (name) =>
-          /node_modules/.test(name) && !/node_modules\/(query-string|filter-obj)/.test(name),
+        include: /node_modules\/(query-string|filter-obj)/,
         use: {
           loader: 'babel-loader',
           options: require('./.babel.config.json'),
