d7a904f8c8d76a9369f862d6543b8b76ad9ddf3e
栗嘉男 <574980606@qq.com>
2023-04-21 10:04:31 +0800

    fix: add Number.isNaN()

--- packages/hooks/src/utils/index.ts
+++ packages/hooks/src/utils/index.ts
@@ -5,5 +5,6 @@ export const isFunction = (value: unknown): value is (...args: any) => any =>
 
 export const isString = (value: unknown): value is string => typeof value === 'string';
 export const isBoolean = (value: unknown): value is boolean => typeof value === 'boolean';
-export const isNumber = (value: unknown): value is number => typeof value === 'number';
+export const isNumber = (value: unknown): value is number =>
+  typeof value === 'number' && !Number.isNaN(value);
 export const isUndef = (value: unknown): value is undefined => typeof value === 'undefined';
