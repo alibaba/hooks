94f842fb49a69ccf6c44aa439e8e3bf3723c9af6
streaker303 <49198429+streaker303@users.noreply.github.com>
2023-07-05 19:55:46 +0800

    Update index.tsx

--- packages/hooks/src/useDeepCompareEffect/index.tsx
+++ packages/hooks/src/useDeepCompareEffect/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
 import { createDeepCompareEffect } from '../createDeepCompareEffect';
 
+// 将高阶函数执行一次，导出内部修改过依赖项的hooks函数
 export default createDeepCompareEffect(useEffect);
