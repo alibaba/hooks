a6313e9d398d621d92e65c2b617e2baf72f7d410
lxr <1076629390@qq.com>
2023-07-05 19:56:32 +0800

    fix: useDebounce 在小程序里使用问题 (#1472)
    * fix: useDebounce 在小程序里使用问题
    
    * chore: 优化声明
    
    ---------
    
    Co-authored-by: 云泥 <liuyibo@xylink.com>
    Co-authored-by: huangcheng <huangcheng.lq@bytedance.com>

--- packages/hooks/src/useDebounceFn/index.ts
+++ packages/hooks/src/useDebounceFn/index.ts
@@ -1,4 +1,4 @@
-import { debounce } from 'lodash-es';
+import { debounce } from '../utils/lodash-polyfill';
 import { useMemo } from 'react';
 import type { DebounceOptions } from '../useDebounce/debounceOptions';
 import useLatest from '../useLatest';
