284d59ae5401715181c8cc6e330a6128cfcbc401
wupanyan <wupanyan@corp.netease.com>
2023-07-31 12:45:52 +0800

    fix(useLockFn): ensure the stability of the output function references of useLockFn

--- packages/hooks/src/useLockFn/index.ts
+++ packages/hooks/src/useLockFn/index.ts
@@ -1,23 +1,20 @@
 import { useRef, useCallback } from 'react';
-
+import useMemoizedFn from '../useMemoizedFn';
 function useLockFn<P extends any[] = any[], V extends any = any>(fn: (...args: P) => Promise<V>) {
   const lockRef = useRef(false);
 
-  return useCallback(
-    async (...args: P) => {
-      if (lockRef.current) return;
-      lockRef.current = true;
-      try {
-        const ret = await fn(...args);
-        lockRef.current = false;
-        return ret;
-      } catch (e) {
-        lockRef.current = false;
-        throw e;
-      }
-    },
-    [fn],
-  );
+  return useMemoizedFn(async (...args: P) => {
+    if (lockRef.current) return;
+    lockRef.current = true;
+    try {
+      const ret = await fn(...args);
+      lockRef.current = false;
+      return ret;
+    } catch (e) {
+      lockRef.current = false;
+      throw e;
+    }
+  });
 }
 
 export default useLockFn;
