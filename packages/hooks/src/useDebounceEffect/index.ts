74c7020e3f60bd5a4fd17d8bd3377025979d9ba6
Damon Feng <245211612@qq.com>
2023-12-12 22:02:35 +0800

    perf(useDebounceEffect): effectCallback called that do not rely on rerender

--- packages/hooks/src/useDebounceEffect/index.ts
+++ packages/hooks/src/useDebounceEffect/index.ts
@@ -1,25 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import type { DependencyList, EffectCallback } from 'react';
 import type { DebounceOptions } from '../useDebounce/debounceOptions';
 import useDebounceFn from '../useDebounceFn';
-import useUpdateEffect from '../useUpdateEffect';
+import { isFunction } from '../utils';
+import useUnmount from '../useUnmount';
+import isDev from '../utils/isDev';
 
 function useDebounceEffect(
   effect: EffectCallback,
   deps?: DependencyList,
   options?: DebounceOptions,
 ) {
-  const [flag, setFlag] = useState({});
+  if (isDev) {
+    if (!isFunction(effect)) {
+      console.error(
+        `useDebounceEffect expected first parameter is a function, got ${typeof effect}`,
+      );
+    }
+  }
+
+  const cleanup = useRef<ReturnType<EffectCallback>>();
 
   const { run } = useDebounceFn(() => {
-    setFlag({});
+    if (isFunction(cleanup.current)) {
+      cleanup.current();
+    }
+    cleanup.current = effect();
   }, options);
 
   useEffect(() => {
     return run();
   }, deps);
 
-  useUpdateEffect(effect, [flag]);
+  useUnmount(() => {
+    if (isFunction(cleanup.current)) {
+      cleanup.current();
+    }
+  });
 }
 
 export default useDebounceEffect;
