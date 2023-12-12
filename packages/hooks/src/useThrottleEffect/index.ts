77ceb50515856721b34de9c36f5ecccae419b94b
Damon Feng <245211612@qq.com>
2023-12-12 22:02:56 +0800

    perf(useThrottleEffect): effectCallback called that do not rely on rerender

--- packages/hooks/src/useThrottleEffect/index.ts
+++ packages/hooks/src/useThrottleEffect/index.ts
@@ -1,25 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import type { DependencyList, EffectCallback } from 'react';
 import type { ThrottleOptions } from '../useThrottle/throttleOptions';
 import useThrottleFn from '../useThrottleFn';
-import useUpdateEffect from '../useUpdateEffect';
+import isDev from '../utils/isDev';
+import { isFunction } from '../utils';
+import useUnmount from '../useUnmount';
 
 function useThrottleEffect(
   effect: EffectCallback,
   deps?: DependencyList,
   options?: ThrottleOptions,
 ) {
-  const [flag, setFlag] = useState({});
+  if (isDev) {
+    if (!isFunction(effect)) {
+      console.error(
+        `useThrottleEffect expected first parameter is a function, got ${typeof effect}`,
+      );
+    }
+  }
+
+  const cleanup = useRef<ReturnType<EffectCallback>>();
 
   const { run } = useThrottleFn(() => {
-    setFlag({});
+    if (isFunction(cleanup.current)) {
+      cleanup.current();
+    }
+    cleanup.current = effect();
   }, options);
 
   useEffect(() => {
     return run();
   }, deps);
 
-  useUpdateEffect(effect, [flag]);
+  useUnmount(() => {
+    if (isFunction(cleanup.current)) {
+      cleanup.current();
+    }
+  });
 }
 
 export default useThrottleEffect;
