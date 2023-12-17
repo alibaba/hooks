292c9f0a63977d406ff50344b9202876b6dff1af
wuxh <wxh1220@gmail.com>
2023-12-17 21:09:47 +0800

    chore: add jsdoc

--- packages/hooks/src/useUpdateEffect/index.ts
+++ packages/hooks/src/useUpdateEffect/index.ts
@@ -1,4 +1,8 @@
 import { useEffect } from 'react';
 import { createUpdateEffect } from '../createUpdateEffect';
 
+/**
+ * A hook alike `useEffect` but skips running the effect for the first time.
+ * @see https://ahooks.js.org/hooks/use-update-effect
+ */
 export default createUpdateEffect(useEffect);
