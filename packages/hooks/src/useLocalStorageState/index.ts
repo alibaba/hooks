7703b718f4523987e1328a4432abe85da8c36596
wuxh <wxh1220@gmail.com>
2023-12-17 20:03:07 +0800

    chore: add jsdoc

--- packages/hooks/src/useLocalStorageState/index.ts
+++ packages/hooks/src/useLocalStorageState/index.ts
@@ -1,6 +1,10 @@
 import { createUseStorageState } from '../createUseStorageState';
 import isBrowser from '../utils/isBrowser';
 
+/**
+ * A Hook that store state into localStorage.
+ * @see https://ahooks.js.org/hooks/use-local-storage-state
+ */
 const useLocalStorageState = createUseStorageState(() => (isBrowser ? localStorage : undefined));
 
 export default useLocalStorageState;
