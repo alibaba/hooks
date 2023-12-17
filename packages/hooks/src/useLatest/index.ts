979c25ec462d063457f4dc1938500dc0ba513234
wuxh <wxh1220@gmail.com>
2023-12-17 20:02:23 +0800

    chore: add jsdoc

--- packages/hooks/src/useLatest/index.ts
+++ packages/hooks/src/useLatest/index.ts
@@ -1,5 +1,9 @@
 import { useRef } from 'react';
 
+/**
+ * A Hook that returns the latest value, effectively avoiding the closure problem.
+ * @see https://ahooks.js.org/hooks/use-latest
+ */
 function useLatest<T>(value: T) {
   const ref = useRef(value);
   ref.current = value;
