f34d462dde88057e611b9dbc927ba31a0cedccc2
wuxh <wxh1220@gmail.com>
2023-12-17 19:21:39 +0800

    chore: add jsdoc

--- packages/hooks/src/useDeepCompareLayoutEffect/index.tsx
+++ packages/hooks/src/useDeepCompareLayoutEffect/index.tsx
@@ -1,4 +1,8 @@
 import { useLayoutEffect } from 'react';
 import { createDeepCompareEffect } from '../createDeepCompareEffect';
 
+/**
+ * Usage is the same as `useLayoutEffect`, but deps are compared with [lodash.isEqual](https://lodash.com/docs/4.17.15#isEqual).
+ * @see https://ahooks.js.org/hooks/use-deep-compare-layout-effect
+ */
 export default createDeepCompareEffect(useLayoutEffect);
