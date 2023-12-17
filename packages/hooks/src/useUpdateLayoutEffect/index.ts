9f822c30c9b756a0a08fe27efcaa33fa24ff0db0
wuxh <wxh1220@gmail.com>
2023-12-17 21:10:44 +0800

    chore: add jsdoc

--- packages/hooks/src/useUpdateLayoutEffect/index.ts
+++ packages/hooks/src/useUpdateLayoutEffect/index.ts
@@ -1,4 +1,8 @@
 import { useLayoutEffect } from 'react';
 import { createUpdateEffect } from '../createUpdateEffect';
 
+/**
+ * A hook alike `useLayoutEffect` but skips running the effect for the first time.
+ * @see https://ahooks.js.org/hooks/use-update-layout-effect
+ */
 export default createUpdateEffect(useLayoutEffect);
