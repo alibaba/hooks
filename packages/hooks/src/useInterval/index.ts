18253fedb625bcaca4f79ee209822e74b27a4f1f
wuxh <wxh1220@gmail.com>
2023-12-17 19:50:47 +0800

    chore: add jsdoc

--- packages/hooks/src/useInterval/index.ts
+++ packages/hooks/src/useInterval/index.ts
@@ -2,6 +2,10 @@ import { useCallback, useEffect, useRef } from 'react';
 import useMemoizedFn from '../useMemoizedFn';
 import { isNumber } from '../utils';
 
+/**
+ * A hook that handles the `setInterval` timer function.
+ * @see https://ahooks.js.org/hooks/use-interval
+ */
 const useInterval = (fn: () => void, delay?: number, options: { immediate?: boolean } = {}) => {
   const timerCallback = useMemoizedFn(fn);
   const timerRef = useRef<NodeJS.Timer | null>(null);
