e29839c4ab41121f69fd9ef002a284a0a80b48e6
JacobZha <zhayang@xizhi-ai.com>
2024-02-27 18:56:38 +0800

    test(useIsScrolling/test): 单测\用例更新

--- packages/hooks/src/useIsScrolling/__tests__/index.test.ts
+++ packages/hooks/src/useIsScrolling/__tests__/index.test.ts
@@ -1,9 +1,26 @@
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import useIsScrolling from '../index';
 
+jest.mock('../../utils/domTarget', () => ({
+  getTargetElement: jest.fn(),
+}));
+jest.mock('../../utils/useEffectWithTarget', () => jest.fn());
+jest.mock('../../useLatest', () => jest.fn(() => ({ current: false })));
+jest.mock('../../utils/rafTimer', () => ({
+  clearRafTimeout: jest.fn(),
+  setRafTimeout: (callback: () => void, delay: number) => {
+    setTimeout(callback, delay);
+    return { id: Math.random() };
+  },
+}));
+
 describe('useIsScrolling', () => {
-  it('document body', () => {
-    const hook = renderHook(() => useIsScrolling({ target: document }));
-    expect(hook.result.current.scrolling).toBeNull();
+  it('should correctly detect scrolling', async () => {
+    const { result } = renderHook(() => useIsScrolling());
+    act(() => {
+      window.dispatchEvent(new Event('scroll'));
+      jest.advanceTimersByTime(100);
+    });
+    expect(result.current.scrolling).toBe(false);
   });
 });
