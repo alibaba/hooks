05282886ee256b54ed47440f605d91e5f680342e
云泥 <1656081615@qq.com>
2024-04-11 09:27:04 +0800

    chore: resolve build error (#2512)

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,7 +4,7 @@ const packages = require('./packages/hooks/package.json');
 
 export default defineConfig({
   mfsu: false,
-  ssr: process.env.NODE_ENV === 'development' ? false : {},
+  ssr: false,
   manifest: {},
   publicPath: '/',
   alias: {
