1eb16eb606bd4e04a50ff5d3efd40a9bd4770bdf
ice <1597834867@qq.com>
2024-04-21 16:18:16 +0800

    feat: init v4

---
+++ .umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/ice/Desktop/learn/hooks/node_modules/.pnpm/@umijs+runtime@3.5.37_react@16.14.0/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
+});
+
+export { plugin };
