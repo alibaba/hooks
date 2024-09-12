c262210c118c563dd6fe6308f2eed969c5474a90
jingzouzou <827088092@qq.com>
2024-09-12 23:41:45 +0800

    chore: delete unuse codes

--- packages/hooks/src/useInfiniteScroll/demo/scroll.tsx
+++ packages/hooks/src/useInfiniteScroll/demo/scroll.tsx
@@ -8,15 +8,10 @@ interface Result {
 
 const resultData = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'];
 
-setInterval(() => {
-  resultData.push(resultData.length.toString());
-}, 10000);
-
-function getLoadMoreList(latestId: number | undefined, limit: number): Promise<Result> {
-  console.log('getLoadMoreList: ', resultData.length);
+function getLoadMoreList(nextId: string | undefined, limit: number): Promise<Result> {
   let start = 0;
-  if (latestId) {
-    start = resultData.findIndex((_, index) => index === latestId) + 1;
+  if (nextId) {
+    start = resultData.findIndex((i) => i === nextId);
   }
   const end = start + limit;
   const list = resultData.slice(start, end);
@@ -34,12 +29,11 @@ function getLoadMoreList(latestId: number | undefined, limit: number): Promise<R
 export default () => {
   const ref = useRef<HTMLDivElement>(null);
 
-  const { data, loading, loadMore, loadingMore, noMore, trulyNoMore } = useInfiniteScroll(
-    (d) => getLoadMoreList(d?.list?.length, 4),
+  const { data, loading, loadMore, loadingMore, noMore } = useInfiniteScroll(
+    (d) => getLoadMoreList(d?.nextId, 4),
     {
       target: ref,
       isNoMore: (d) => d?.nextId === undefined,
-      forcedLoadMore: true,
     },
   );
 
@@ -51,7 +45,7 @@ export default () => {
         <div>
           {data?.list?.map((item) => (
             <div key={item} style={{ padding: 12, border: '1px solid #f5f5f5' }}>
-              ite32132m-{item}
+              item-{item}
             </div>
           ))}
         </div>
@@ -64,7 +58,7 @@ export default () => {
           </button>
         )}
 
-        {trulyNoMore && <span>No more data</span>}
+        {noMore && <span>No more data</span>}
       </div>
     </div>
   );
