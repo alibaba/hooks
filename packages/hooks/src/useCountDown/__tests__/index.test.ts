f1421f68b062160bcddc8234b9ea2112034d44ad
try <try418@163.com>
2024-12-31 16:22:44 +0800

    fix: 修复useAntdTable在manual=true时，初始化仍然发起请求的问题

--- packages/hooks/src/useCountDown/__tests__/index.test.ts
+++ packages/hooks/src/useCountDown/__tests__/index.test.ts
@@ -1,4 +1,5 @@
-import { act, renderHook } from '@testing-library/react';
+import { renderHook } from '@testing-library/react';
+import { act } from 'react';
 import type { Options } from '../index';
 import useCountDown from '../index';
 
--- packages/hooks/src/useEventTarget/__tests__/index.test.ts
+++ packages/hooks/src/useEventTarget/__tests__/index.test.ts
@@ -1,4 +1,5 @@
-import { renderHook, act } from '@testing-library/react';
+import { renderHook } from '@testing-library/react';
+import { act } from 'react';
 import useEventTarget from '../index';
 
 describe('useEventTarget', () => {
