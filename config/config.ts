d1bf0f8583a2079efe7c07adfbd626d0286754ca
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2025-03-05 10:39:32 +0800

    refactor(lint): f2elint

--- config/config.ts
+++ config/config.ts
@@ -38,8 +38,8 @@ export default {
   manifest: {},
   hash: true,
   alias: {
-    ahooks: process.cwd() + '/packages/hooks/src/index.ts',
-    '@ahooksjs/use-url-state': process.cwd() + '/packages/use-url-state/src/index.ts',
+    ahooks: `${process.cwd()}/packages/hooks/src/index.ts`,
+    '@ahooksjs/use-url-state': `${process.cwd()}/packages/use-url-state/src/index.ts`,
   },
   resolve: {
     includes: ['docs', 'packages/hooks/src', 'packages/use-url-state'],
