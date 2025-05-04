0475849f2acb812c338734ea7918c0a119b0b154
Jianan Li <jianan@orderly.network>
2025-05-05 04:00:36 +0800

    fix: fix

--- packages/hooks/src/useControllableValue/__tests__/index.test.ts
+++ packages/hooks/src/useControllableValue/__tests__/index.test.ts
@@ -1,28 +1,28 @@
-import { act, renderHook } from "@testing-library/react";
-import type { Options, Props } from "../index";
-import useControllableValue from "../index";
+import { act, renderHook } from '@testing-library/react';
+import type { Options, Props } from '../index';
+import useControllableValue from '../index';
 
-describe("useControllableValue", () => {
+describe('useControllableValue', () => {
   const setUp = (props?: Props, options?: Options<any>) =>
     renderHook(() => useControllableValue(props, options));
 
-  it("defaultValue should work", () => {
+  it('defaultValue should work', () => {
     const hook = setUp({ defaultValue: 1 });
     expect(hook.result.current[0]).toBe(1);
   });
 
-  it("value should work", () => {
+  it('value should work', () => {
     const hook = setUp({ defaultValue: 1, value: 2 });
     expect(hook.result.current[0]).toBe(2);
   });
 
-  it("state should be undefined", () => {
+  it('state should be undefined', () => {
     const hook = setUp();
     expect(hook.result.current[0]).toBeUndefined();
   });
 
-  it("onChange should work", () => {
-    let extraParam: string = "";
+  it('onChange should work', () => {
+    let extraParam: string = '';
     const props = {
       value: 2,
       onChange(v: any, extra) {
@@ -33,13 +33,13 @@ describe("useControllableValue", () => {
     const hook = setUp(props);
     expect(hook.result.current[0]).toBe(2);
     act(() => {
-      hook.result.current[1](3, "extraParam");
+      hook.result.current[1](3, 'extraParam');
     });
     expect(props.value).toBe(3);
-    expect(extraParam).toBe("extraParam");
+    expect(extraParam).toBe('extraParam');
   });
 
-  it("test on state update", () => {
+  it('test on state update', () => {
     const props: any = {
       value: 1,
     };
@@ -52,7 +52,7 @@ describe("useControllableValue", () => {
     expect(result.current[0]).toBe(3);
   });
 
-  it("test set state", async () => {
+  it('test set state', async () => {
     const { result } = setUp({
       newValue: 1,
     });
@@ -70,7 +70,7 @@ describe("useControllableValue", () => {
     expect(result.current[0]).toBe(56);
   });
 
-  it("type inference should work", async () => {
+  it('type inference should work', async () => {
     type Value = {
       foo: number;
     };
