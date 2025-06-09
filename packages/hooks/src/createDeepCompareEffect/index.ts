03364993c342d3663b984ce8bb799c46f6cfbc6a
Jianan Li <jianan@orderly.network>
2025-06-09 22:48:26 +0800

    fix: fix

--- packages/hooks/src/createDeepCompareEffect/index.ts
+++ packages/hooks/src/createDeepCompareEffect/index.ts
@@ -1,22 +1,16 @@
-import {
-  useRef,
-  type DependencyList,
-  type useEffect,
-  type useLayoutEffect,
-} from "react";
-import { depsEqual } from "../utils/depsEqual";
+import { useRef, type DependencyList, type useEffect, type useLayoutEffect } from 'react';
+import { depsEqual } from '../utils/depsEqual';
 
 type EffectHookType = typeof useEffect | typeof useLayoutEffect;
 
 type CreateUpdateEffect = (hook: EffectHookType) => EffectHookType;
 
-export const createDeepCompareEffect: CreateUpdateEffect =
-  (hook) => (effect, deps) => {
-    const ref = useRef<DependencyList>(undefined);
-    const signalRef = useRef<number>(0);
-    if (deps === undefined || !depsEqual(deps, ref.current)) {
-      signalRef.current += 1;
-    }
-    ref.current = deps;
-    hook(effect, [signalRef.current]);
-  };
+export const createDeepCompareEffect: CreateUpdateEffect = (hook) => (effect, deps) => {
+  const ref = useRef<DependencyList>(undefined);
+  const signalRef = useRef<number>(0);
+  if (deps === undefined || !depsEqual(deps, ref.current)) {
+    signalRef.current += 1;
+  }
+  ref.current = deps;
+  hook(effect, [signalRef.current]);
+};
