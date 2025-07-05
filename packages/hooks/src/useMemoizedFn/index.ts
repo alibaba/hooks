6f450f37a3098e23d115f5c0a478656d8d30775e
黎真真 <3253016187@qq.com>
2025-07-06 06:51:42 +0800

    chore: update devDependencies

--- packages/hooks/src/useMemoizedFn/index.ts
+++ packages/hooks/src/useMemoizedFn/index.ts
@@ -1,6 +1,6 @@
-import { useMemo, useRef } from 'react';
-import { isFunction } from '../utils';
-import isDev from '../utils/isDev';
+import { useMemo, useRef } from "react";
+import { isFunction } from "../utils";
+import isDev from "../utils/isDev";
 
 type noop = (this: any, ...args: any[]) => any;
 
@@ -9,10 +9,12 @@ type PickFunction<T extends noop> = (
   ...args: Parameters<T>
 ) => ReturnType<T>;
 
-function useMemoizedFn<T extends noop>(fn: T) {
+const useMemoizedFn = <T extends noop>(fn: T) => {
   if (isDev) {
     if (!isFunction(fn)) {
-      console.error(`useMemoizedFn expected parameter is a function, got ${typeof fn}`);
+      console.error(
+        `useMemoizedFn expected parameter is a function, got ${typeof fn}`
+      );
     }
   }
 
@@ -31,6 +33,6 @@ function useMemoizedFn<T extends noop>(fn: T) {
   }
 
   return memoizedFn.current;
-}
+};
 
 export default useMemoizedFn;
