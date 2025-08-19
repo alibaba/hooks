68ddd0b462f4fbaceedc67e87f760549d84f6166
遇见同学 <1875694521@qq.com>
2025-08-19 22:25:08 +0800

    chore: test cmd improve

--- packages/use-url-state/vitest.config.ts
+++ packages/use-url-state/vitest.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     },
   },
   test: {
+    reporters: ['default', 'verbose'],
     environment: 'jsdom',
     include: ['__tests__/**/*.spec.ts?(x)'],
     coverage: {
