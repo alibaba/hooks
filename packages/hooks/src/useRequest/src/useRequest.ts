f031da8c2c031d16e5aec52d8d196536bb10de63
arktomson <2372929539@qq.com>
2026-02-18 23:03:51 +0800

    fix(useRequest): 修复默认参数场景的类型推导
    - 新增 useRequest 基于 service 的重载，推导参数与返回值类型
    
    - 修复默认参数函数中参数被推导为 any 的问题并补充回归测试
    
    Fixes #2671

--- packages/hooks/src/useRequest/src/useRequest.ts
+++ packages/hooks/src/useRequest/src/useRequest.ts
@@ -6,8 +6,14 @@ import usePollingPlugin from './plugins/usePollingPlugin';
 import useRefreshOnWindowFocusPlugin from './plugins/useRefreshOnWindowFocusPlugin';
 import useRetryPlugin from './plugins/useRetryPlugin';
 import useThrottlePlugin from './plugins/useThrottlePlugin';
-import type { Options, Plugin, Service } from './types';
+import type { Options, Plugin, Result, Service } from './types';
 import useRequestImplement from './useRequestImplement';
+
+type AnyService = (...args: any[]) => Promise<any>;
+
+type InferData<TService extends AnyService> = Awaited<ReturnType<TService>>;
+
+type InferParams<TService extends AnyService> = Parameters<TService>;
 
 // function useRequest<TData, TParams extends any[], TFormated, TTFormated extends TFormated = any>(
 //   service: Service<TData, TParams>,
@@ -19,6 +25,18 @@ import useRequestImplement from './useRequestImplement';
 //   options?: OptionsWithoutFormat<TData, TParams>,
 //   plugins?: Plugin<TData, TParams>[],
 // ): Result<TData, TParams>
+function useRequest<TService extends AnyService>(
+  service: TService,
+  options?: Options<InferData<TService>, InferParams<TService>>,
+  plugins?: Plugin<InferData<TService>, InferParams<TService>>[],
+): Result<InferData<TService>, InferParams<TService>>;
+
+function useRequest<TData, TParams extends any[]>(
+  service: Service<TData, TParams>,
+  options?: Options<TData, TParams>,
+  plugins?: Plugin<TData, TParams>[],
+): Result<TData, TParams>;
+
 function useRequest<TData, TParams extends any[]>(
   service: Service<TData, TParams>,
   options?: Options<TData, TParams>,
