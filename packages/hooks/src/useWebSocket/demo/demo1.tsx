92a6a68f89a7ba60a92340f27b40a66d70819df9
lijianan <574980606@qq.com>
2026-04-07 08:59:31 +0800

    chore: clear

--- packages/hooks/src/useWebSocket/demo/demo1.tsx
+++ packages/hooks/src/useWebSocket/demo/demo1.tsx
@@ -1,5 +1,5 @@
-import React, { useRef, useMemo } from "react";
-import { useWebSocket } from "ahooks";
+import React, { useRef, useMemo } from 'react';
+import { useWebSocket } from 'ahooks';
 
 enum ReadyState {
   Connecting = 0,
@@ -11,15 +11,13 @@ enum ReadyState {
 const Demo: React.FC = () => {
   const messageHistory = useRef<MessageEvent<any>[]>([]);
 
-  const { readyState, latestMessage, sendMessage, disconnect, connect } =
-    useWebSocket("wss://ws.postman-echo.com/raw");
+  const { readyState, latestMessage, sendMessage, disconnect, connect } = useWebSocket(
+    'wss://ws.postman-echo.com/raw',
+  );
 
   messageHistory.current = useMemo(
-    () =>
-      latestMessage
-        ? messageHistory.current.concat(latestMessage)
-        : messageHistory.current,
-    [latestMessage]
+    () => (latestMessage ? messageHistory.current.concat(latestMessage) : messageHistory.current),
+    [latestMessage],
   );
 
   return (
@@ -41,17 +39,14 @@ const Demo: React.FC = () => {
         ❌ disconnect
       </button>
       {/* connect */}
-      <button
-        onClick={() => connect?.()}
-        disabled={readyState === ReadyState.Open}
-      >
-        {readyState === ReadyState.Connecting ? "connecting" : "📞 connect"}
+      <button onClick={() => connect?.()} disabled={readyState === ReadyState.Open}>
+        {readyState === ReadyState.Connecting ? 'connecting' : '📞 connect'}
       </button>
       <div style={{ marginTop: 8 }}>readyState: {readyState}</div>
       <div style={{ marginTop: 8 }}>
         <p>received message: </p>
         {messageHistory.current.map((message, index) => (
-          <p key={index} style={{ wordWrap: "break-word" }}>
+          <p key={index} style={{ wordWrap: 'break-word' }}>
             {message?.data}
           </p>
         ))}
